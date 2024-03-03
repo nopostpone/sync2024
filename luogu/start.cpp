13841b73368a62107b3d9932a3b7c6c6b190fbbf
Csus4UwU <126860030+Csus4UwU@users.noreply.github.com>
2024-03-03 09:03:55 +0800

    1

---
+++ luogu/start.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+using ll = long long;
+
+const int N = 1e5 + 10;
+// priority_queue<ll, vector<ll>, greater<ll>> pq;
+// ll x;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    while (_--)
+        solve();
+    return 0;
+}
