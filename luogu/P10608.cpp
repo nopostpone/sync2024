68bfd84a07f496925092d44f55c714e761eaae28
Csus4 <2334085989@qq.com>
2024-06-17 23:13:12 +0800

    <udpate> P10608

--- luogu/P10608.cpp
+++ luogu/P10608.cpp
@@ -1,37 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-template <typename T>
-void read(T &x)
-{
-    x = 0;
-    int f = 1;
-    char ch = getchar();
-    while (!isdigit(ch))
-    {
-        if (ch == '-')
-            f = -1;
-        ch = getchar();
-    }
-    while (isdigit(ch))
-    {
-        x = x * 10 + (ch ^ 48);
-        ch = getchar();
-    }
-    x *= f;
-}
-const int MAX = 2e5 + 5;
-char seq[MAX], d[MAX];
+using ll = long long;
+const int N = 2e5 + 5;
+
+char seq[N], d[N];
 struct Modify
 {
     char who;
     int pos;
-} q[MAX];
+} q[N];
+
 int n, m;
-signed main()
+ll res = 1;
+
+void solve()
 {
     queue<int> qu;
-    read(n), read(m);
+    cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
         cin >> seq[i];
@@ -40,8 +25,7 @@ signed main()
     }
     for (int i = 1; i <= m; i++)
     {
-        cin >> q[i].who;
-        read(q[i].pos);
+        cin >> q[i].who >> q[i].pos;
         d[q[i].pos] = q[i].who;
     }
     if (qu.empty())
@@ -56,41 +40,32 @@ signed main()
         if (x - 1 >= 1 && x - 1 <= n && seq[x - 1] == '_')
         {
             if (d[x - 1] == 'R')
-            {
-                if (seq[x] == 'W')
-                    seq[x - 1] = 'B';
-                else
-                    seq[x - 1] = 'W';
-            }
+                seq[x - 1] = (seq[x] == 'W') ? 'B' : 'W';
             else
-            {
                 seq[x - 1] = seq[x];
-            }
             qu.push(x - 1);
         }
         if (x + 1 >= 1 && x + 1 <= n && seq[x + 1] == '_')
         {
             if (d[x + 1] == 'R')
-            {
-                if (seq[x] == 'W')
-                    seq[x + 1] = 'B';
-                else
-                    seq[x + 1] = 'W';
-            }
+                seq[x + 1] = (seq[x] == 'W') ? 'B' : 'W';
             else
-            {
                 seq[x + 1] = seq[x];
-            }
             qu.push(x + 1);
         }
     }
-    int ans = 1;
     for (int i = 2; i <= n; i++)
     {
         if (seq[i] == seq[i - 1])
             continue;
-        ans++;
+        res++;
     }
-    cout << ans << endl;
+    cout << res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    solve();
     return 0;
 }
