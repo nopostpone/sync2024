a9e8c85e3e54760efc653f832445d1f7d884cb60
Csus4 <2334085989@qq.com>
2024-06-22 02:19:16 +0800

    <update> P10607

--- luogu/P10607.cpp
+++ luogu/P10607.cpp
@@ -1,62 +1,67 @@
 // https://www.luogu.com.cn/problem/P10607
+// 不会做的贪心
+// 思路来自： https://www.luogu.com.cn/article/yc3rjfhz
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define endl '\n'
-#define enter putchar('\n')
 const int N = 2e5 + 100;
-
-inline char nc()
-{
-    static char buf[100000], *p1 = buf, *p2 = buf;
-    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++;
-}
-inline ll read()
-{
-    int x = 0, f = 1;
-    char ch = nc();
-    while (ch < '0' || ch > '9')
-    {
-        if (ch == '-')
-            f = -1;
-        ch = nc();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        x = x * 10 + (ch ^ 48);
-        ch = nc();
-    }
-    return x * f;
-}
-void write(ll x)
-{
-    if (x < 0)
-        putchar('-'), x = -x;
-    if (x >= 10)
-        write(x / 10);
-    putchar(x % 10 + '0');
-}
 
 struct node
 {
     int x, y, k;
-} q[N];
+} p[N], q[N];
 
 bool cmp1(const node &a, const node &b)
 {
-    return a.x > b.x;
+    return a.x == b.x ? a.y < b.y : a.x < b.x;
 }
 
 bool cmp2(const node &a, const node &b)
 {
-    return a.y > b.y;
+    return a.y == b.y ? a.k > b.k : a.y < b.y;
 }
+
+int n, m;
+int a[N], qq[N], pp[N];
+ll res;
 
 void solve()
 {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= m; i++)
+        cin >> q[i].x >> q[i].y >> q[i].k;
+    for (int i = 1; i <= m; i++)
+        p[i] = q[i];
+
+    sort(p + 1, p + 1 + m, cmp1), sort(q + 1, q + 1 + m, cmp2);
+    pp[n] = a[n];
+    for (int i = n - 1; i; i--)
+        pp[i] = min(pp[i + 1], a[i]);
+    qq[1] = a[1];
+    for (int i = 2; i <= n; i++)
+        qq[i] = min(qq[i - 1], a[i]);
+
+    int r = m, l = 1, k = 0;
+    while (r >= 1)
+    {
+        int nr = p[r].k - k, nl = q[l].k - k;
+        int nk = min(nl, nr);
+        res += ll(nk) * ll(pp[p[r].x]);
+        res += ll(nk) * ll(qq[q[l].y]);
+        k += nk;
+        while (p[r].k <= k and r >= 1)
+            r--;
+        while (q[l].k <= k and l <= m)
+            l++;
+    }
+    res -= qq[q[1].y];
+    cout << res << endl;
 }
 
-int main()
+signed main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int _ = 1;
