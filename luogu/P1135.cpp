e5102bcd2f069bad4b93e1f8de222d51b25e44ad
Csus4 <2334085989@qq.com>
2024-06-25 18:29:11 +0800

    <new> P1135

---
+++ luogu/P1135.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define enter putchar('\n')
+const int N = 1e5 + 100;
+
+int res,n,st0,ed0;
+int g[N];
+
+void dfs()
+{
+    
+}
+
+void solve()
+{
+    cin>>n>>st0>>ed0;
+    for(int i=1;i<=n;i++)
+        cin>>g[i];
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    while (_--)
+        solve();
+    return 0;
+}
