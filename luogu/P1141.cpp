7f42b9210b845c6b9cb7611e56c3080542b41716
Csus4 <2334085989@qq.com>
2024-06-25 20:42:49 +0800

    <update> P1141 <new> P1331

--- luogu/P1141.cpp
+++ luogu/P1141.cpp
@@ -1,3 +1,5 @@
+// https://www.luogu.com.cn/problem/P1141
+// 不 要 memset
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -7,11 +9,10 @@ const int N = 1e3 + 100;
 const int M = 1e5 + 100;
 
 bool mp[N][N];
-ll ans[N][N];
+ll ans[N * N];
 int id[N][N];
 int dx[5] = {0, 1, -1, 0, 0};
 int dy[5] = {0, 0, 0, 1, -1};
-bool vis[N][N];
 int n, m;
 pair<int, int> query[M];
 
@@ -32,28 +33,26 @@ bool isout(int x, int y)
     return false;
 }
 
-void bfs(int a, int b)
+void bfs(int a, int b, int idx)
 {
-    memset(vis, 0, sizeof(vis));
     ll res = 0;
     queue<pair<int, int>> qe;
     qe.push({a, b});
-    vis[a][b] = true;
+    id[a][b] = idx;
     while (!qe.empty())
     {
         int x = qe.front().first, y = qe.front().second;
-        // cout<<"rudui "<<x<<' '<<y<<endl;
         int nx, ny;
         res++;
         for (int i = 1; i <= 4; i++)
         {
             nx = x + dx[i], ny = y + dy[i];
-            if (mp[nx][ny] ^ mp[x][y] and !vis[nx][ny] and !isout(nx, ny))
-                qe.push({nx, ny}), vis[nx][ny] = true;
+            if (mp[nx][ny] ^ mp[x][y] and id[nx][ny] == 0 and !isout(nx, ny))
+                qe.push({nx, ny}), id[nx][ny] = idx;
         }
         qe.pop();
     }
-    cout << res << endl;
+    ans[idx] = res;
 }
 
 void solve()
@@ -75,8 +74,13 @@ void solve()
     // pfdb(1);
     for (int i = 1; i <= m; i++)
         cin >> query[i].first >> query[i].second;
+    int idx = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            if (id[i][j] == 0)
+                bfs(i, j, idx++);
     for (int i = 1; i <= m; i++)
-        bfs(query[i].first, query[i].second);
+        cout << ans[id[query[i].first][query[i].second]] << endl;
 }
 
 int main()
---
+++ luogu/P1331.cpp
@@ -0,0 +1,20 @@
+// https://www.luogu.com.cn/problem/P1331
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define enter putchar('\n')
+const int N = 1e5 + 100;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    while (_--)
+        solve();
+    return 0;
+}
