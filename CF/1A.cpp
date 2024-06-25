2bc1fc235cb5a17141243f56e89744c6a06613c7
Csus4 <2334085989@qq.com>
2024-06-25 13:01:33 +0800

    <update> CF1A

--- CF/1A.cpp
+++ CF/1A.cpp
@@ -6,37 +6,6 @@ using ll = long long;
 #define enter putchar('\n')
 const int N = 1e5 + 100;
 
-inline char nc()
-{
-    static char buf[100000], *p1 = buf, *p2 = buf;
-    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++;
-}
-inline ll read()
-{
-    int x = 0, f = 1;
-    char ch = nc();
-    while (ch < '0' || ch > '9')
-    {
-        if (ch == '-')
-            f = -1;
-        ch = nc();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        x = x * 10 + (ch ^ 48);
-        ch = nc();
-    }
-    return x * f;
-}
-void write(ll x)
-{
-    if (x < 0)
-        putchar('-'), x = -x;
-    if (x >= 10)
-        write(x / 10);
-    putchar(x % 10 + '0');
-}
-
 int res1, res2;
 ll res;
 void solve()
