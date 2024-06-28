cdc28b29de1170e8ad4cfd1339ba5fd7d7f42ed4
Csus4 <2334085989@qq.com>
2024-06-28 17:51:55 +0800

    <update> P2742

--- luogu/P2742.cpp
+++ luogu/P2742.cpp
@@ -6,53 +6,52 @@ using ll = long long;
 #define endl '\n'
 #define enter putchar('\n')
 const int N = 1e5 + 100;
-const double eps=1e-12;
+const double eps = 1e-12;
 
 struct point
 {
-    double x,y;
-    inline double Rand(){return ((double)rand()/RAND_MAX);}
+    double x, y;
+    inline double Rand() { return ((double)rand() / RAND_MAX); }
     inline double RandEps()
     {
-        double RAND=Rand();
-        return (RAND-0.5)*eps;
+        double RAND = Rand();
+        return (RAND - 0.5) * eps;
     }
     inline void Shake()
     {
-        x+=RandEps();
-        y+=RandEps();
+        x += RandEps();
+        y += RandEps();
     }
-    inline point operator-(const point &o)const{return point{x-o.x,y-o.y};}
-    inline point operator+(const point &o)const{return point{x+o.x,y+o.y};}
-    inline double operator^(const point &o)const{return x*o.x+y*o.y;}
-    inline double operator*(const point &o)const{return x*o.y-y*o.x;}
-    inline double getlen(){return sqrt(x*x+y*y);}
-}pt[N];
+    inline point operator-(const point &o) const { return point{x - o.x, y - o.y}; }
+    inline point operator+(const point &o) const { return point{x + o.x, y + o.y}; }
+    inline double operator^(const point &o) const { return x * o.x + y * o.y; }
+    inline double operator*(const point &o) const { return x * o.y - y * o.x; }
+    inline double getlen() { return sqrt(x * x + y * y); }
+} pt[N];
 
 struct line
 {
     int p[2];
     point Normal()
     {
-        point o=pt[p[1]]-pt[p[0]];
-        return point{-1./o.x,1./o.y};
+        point o = pt[p[1]] - pt[p[0]];
+        return point{-1. / o.x, 1. / o.y};
     }
     bool IsRight(const point &o)
     {
-        return (Normal ^ o) >= 0;
+        return (Normal() ^ (o-pt[p[0]])) >= 0;
     }
     double getlen()
     {
-        point o=pt[p[1]]-pt[p[0]];
+        point o = pt[p[1]] - pt[p[0]];
         return o.getlen();
     }
-}l[N],t[N];
+} l[N], t[N];
 
-int n,m;
+int n, m;
 
 void solve()
 {
-    
 }
 
 int main()
