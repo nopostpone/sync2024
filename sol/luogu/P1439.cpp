ee8feae088cbe1d95b0af184574305e33862fc7f
Csus4 <2334085989@qq.com>
2024-06-30 21:00:46 +0800

    <new> P1439

---
+++ sol/luogu/P1439.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define enter putchar('\n')
+const int N = 1e5 + 100;
+
+int n;
+int a[N], b[N], pos[N], d[N];
+
+void solve()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= n; i++)
+        cin >> b[i];
+    for (int i = 1; i <= n; i++)
+        pos[a[i]] = i;
+    int len = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int now = pos[b[i]];
+        if (now > d[len])
+            d[++len] = now;
+        else
+            d[lower_bound(d + 1, d + 1 + len, now) - d] = now;
+    }
+    cout << len;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    while (_--)
+        solve();
+    return 0;
+}
