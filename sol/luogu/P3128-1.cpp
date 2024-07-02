32c1fc81442b898a32d4c7eb3a1097f935b115ab
Csus4 <2334085989@qq.com>
2024-07-03 07:57:53 +0800

    <new> P3128'

---
+++ sol/luogu/P3128-1.cpp
@@ -0,0 +1,44 @@
+// https://www.luogu.com.cn/problem/P3128
+// 树剖+差分 O(nlogn)
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define lst pos << 1
+#define rst pos << 1 | 1
+const int N = 1e5 + 10;
+
+int n,m;
+vector<int> g[N];
+int dep[N], dfn[N], top[N], pre[N], a[N];
+
+void Adfs(int now, int fa)
+{
+
+}
+
+void Bdfs(int now, int tp)
+{
+
+}
+
+void solve()
+{
+    cin >> n >> m;
+    for (int i = 1; i < n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    while (_--)
+        solve();
+    return 0;
+}
