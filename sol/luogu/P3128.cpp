2f605e1059dd9e57f8dc57b1726d68d21ec19531
Csus4 <2334085989@qq.com>
2024-07-03 01:55:26 +0800

    <update> P3128

--- sol/luogu/P3128.cpp
+++ sol/luogu/P3128.cpp
@@ -10,7 +10,7 @@ const int N = 1e5 + 10;
 
 vector<int> g[N];
 int n, root, m;
-int siz[N], top[N], hson[N], pre[N], dep[N], dfn[N], a[N], aa[N], bottom[N];
+int siz[N], top[N], hson[N], pre[N], dep[N], dfn[N], a[N], bottom[N];
 int jiediandfn;
 ll P;
 struct SegmentTreeNode
@@ -45,13 +45,11 @@ void Bdfs(int now, int tp)
     dfn[now] = ++jiediandfn;
     if (siz[now] == 1)
         bottom[now] = dfn[now];
-
     if (hson[now])
     {
         Bdfs(hson[now], tp);
         bottom[now] = bottom[hson[now]];
     }
-
     for (auto &to : g[now])
     {
         if (to == pre[now] or to == hson[now])
@@ -64,14 +62,14 @@ void Bdfs(int now, int tp)
 
 void pushdown(ll pos)
 {
+    s[lst].mx += s[pos].add, s[rst].mx += s[pos].add;
     s[lst].add += s[pos].add, s[rst].add += s[pos].add;
     s[pos].add = 0;
 }
 
 void pushup(ll pos)
 {
-    s[pos].sum = s[lst].sum + s[rst].sum;
-    s[pos].sum %= P;
+    s[pos].mx = max(s[lst].mx, s[rst].mx);
 }
 
 void build(ll l, ll r, ll pos)
@@ -79,7 +77,7 @@ void build(ll l, ll r, ll pos)
     s[pos].l = l, s[pos].r = r, s[pos].add = 0;
     if (l == r)
     {
-        s[pos].sum = a[l] % P;
+        s[pos].mx = a[l];
         return;
     }
     ll m = l + ((r - l) >> 1);
@@ -88,86 +86,40 @@ void build(ll l, ll r, ll pos)
     pushup(pos);
 }
 
-void add(ll x, ll y, ll pos, ll val)
+void add(ll x, ll y, ll pos)
 {
     if (x > y)
         swap(x, y);
     if (x <= s[pos].l and s[pos].r <= y)
     {
-        s[pos].add = (s[pos].add + val) % P;
-        s[pos].sum = (s[pos].sum + (s[pos].r - s[pos].l + 1) * val) % P;
+        s[pos].mx++;
+        s[pos].add++;
         return;
     }
     pushdown(pos);
     ll m = (s[pos].l + s[pos].r) >> 1;
     if (x <= m)
-        add(x, y, lst, val);
+        add(x, y, lst);
     if (y > m)
-        add(x, y, rst, val);
+        add(x, y, rst);
     pushup(pos);
 }
 
-ll getsum(ll x, ll y, ll pos)
-{
-    if (x > y)
-        swap(x, y);
-    if (x <= s[pos].l and s[pos].r <= y)
-    {
-        return (s[pos].sum) % P;
-    }
-    pushdown(pos);
-    ll m = (s[pos].l + s[pos].r) >> 1;
-    ll ans = 0;
-    if (x <= m)
-        ans = (ans + getsum(x, y, lst)) % P;
-    if (y > m)
-        ans = (ans + getsum(x, y, rst)) % P;
-    return ans;
-}
-
-void updatelujing(int u, int v, int val)
+void updatelujing(int u, int v)
 {
     while (top[u] != top[v])
     {
         if (dep[top[u]] < dep[top[v]])
             swap(u, v);
-        add(dfn[top[u]], dfn[u], 1, val);
+        add(dfn[top[u]], dfn[u], 1);
         u = pre[top[u]];
     }
-    add(dfn[u], dfn[v], 1, val);
-}
-
-ll asklujing(int u, int v)
-{
-    ll ans = 0;
-    while (top[u] != top[v])
-    {
-        if (dep[top[u]] < dep[top[v]])
-            swap(u, v);
-        ans += getsum(dfn[top[u]], dfn[u], 1);
-        ans %= P;
-        u = pre[top[u]];
-    }
-    ans += getsum(dfn[u], dfn[v], 1);
-    ans %= P;
-    return ans;
-}
-
-void updatezishu(int x, int z)
-{
-    add(dfn[x], bottom[x], 1, z);
-}
-
-ll askzishu(int x)
-{
-    return getsum(dfn[x], bottom[x], 1) % P;
+    add(dfn[u], dfn[v], 1);
 }
 
 void solve()
 {
-    cin >> n >> m >> root >> P;
-    for (int i = 1; i <= n; i++)
-        cin >> aa[i];
+    cin >> n >> m;
     for (int i = 1; i < n; i++)
     {
         int u, v;
@@ -175,69 +127,16 @@ void solve()
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    Adfs(root, 0);
-    Bdfs(root, root);
-    for (int i = 1; i <= n; i++)
-        a[dfn[i]] = aa[i];
+    Adfs(1, 0);
+    Bdfs(1, 1);
     build(1, n, 1);
     while (m--)
     {
-        int opt, x, y = 0, z = 0;
-        cin >> opt;
-        if (opt == 1)
-        {
-            cin >> x >> y >> z;
-            updatelujing(x, y, z);
-        }
-        else if (opt == 2)
-        {
-            cin >> x >> y;
-            res.push_back(asklujing(x, y));
-        }
-        else if (opt == 3)
-        {
-            cin >> x >> z;
-            updatezishu(x, z);
-        }
-        else if (opt == 4)
-        {
-            cin >> x;
-            res.push_back(askzishu(x));
-        }
-        else
-        {
-            cout << "?" << endl;
-            exit(0);
-        }
+        int x, y;
+        cin >> x >> y;
+        updatelujing(x, y);
     }
-    for (auto &i : res)
-        cout << i % P << endl;
-}
-
-// 调线段树用-P3372
-void tiaoshi()
-{
-    P = 0x3f3f3f3f3f;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> a[i];
-    }
-    build(1, n, 1);
-    for (int i = 1; i <= m; i++)
-    {
-        int opt, x, y, k;
-        cin >> opt >> x >> y;
-        if (opt == 1)
-        {
-            cin >> k;
-            add(x, y, 1, k);
-        }
-        else if (opt == 2)
-            res.push_back(getsum(x, y, 1));
-    }
-    for (auto &i : res)
-        cout << i << endl;
+    cout << s[1].mx;
 }
 
 int main()
