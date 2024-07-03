07605bdc17cdc16dfba6f7b98e27982f7de4ec32
Csus4 <2334085989@qq.com>
2024-07-04 02:13:54 +0800

    <update> P1359 <new> problemset of dp

---
+++ problemset/dp.md
@@ -0,0 +1,3 @@
+# 动态规划 题单
+
+## [入门](https://www.luogu.com.cn/training/1060#problems)
--- sol/luogu/P1359.cpp
+++ sol/luogu/P1359.cpp
@@ -1,3 +1,9 @@
+// https://www.luogu.com.cn/problem/P1359
+// 置f[i][j]为考虑前i条船的情况下，到j出租站最少的花费
+// 在每一个出租站，可以换船或者跳过
+// 例如在(j-1)站——
+// 跳过：f[i][j] = f[i-1][j]
+// 换船（换要从i开始换）：f[i][j] = f[i-1][i]+r[i][j]
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -6,17 +12,7 @@ using ll = long long;
 const int N = 250;
 
 int n;
-int r[N][N], f[N];
-
-int dp()
-{
-    f[2] = r[1][2];
-    for (int i = 3; i <= n; i++)
-    {
-        f[i] = min(f[i - 1] + r[i - 1][i], r[1][i]);
-    }
-    return f[n];
-}
+int r[N][N], f[N][N];
 
 void solve()
 {
@@ -24,7 +20,16 @@ void solve()
     for (int i = 1; i < n; i++)
         for (int j = i + 1; j <= n; j++)
             cin >> r[i][j];
-    cout << dp() << endl;
+    for (int i = 2; i <= n; i++)
+    {
+        f[1][i] = r[1][i];
+    }
+    for (int i = 2; i < n; i++)
+        for (int j = i + 1; j <= n; j++)
+        {
+            f[i][j] = min(f[i - 1][j], f[i - 1][i] + r[i][j]);
+        }
+    cout << f[n - 1][n];
 }
 
 int main()
