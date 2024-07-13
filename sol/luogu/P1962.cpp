2266fe8d4b220e990c8148aeace454bb17bc7ef1
Csus4 <2334085989@qq.com>
2024-07-13 23:04:29 +0800

    <update> P1962

--- sol/luogu/P1962.cpp
+++ sol/luogu/P1962.cpp
@@ -1,3 +1,7 @@
+// https://www.luogu.com.cn/problem/P1962
+// 斐波那契数列 矩阵快速幂 O(logN)
+// fn   =   fn-1 + fn-2     =   1 1 *   fn-1
+// fn-1     fn-1 + 0*fn-2       1 0     fn-2
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -6,28 +10,79 @@ using ll = long long;
 const int N = 1e5 + 100;
 
 ll x;
-ll c[N][N];
+const ll p = 1e9 + 7;
 
-void multi(int a[][N], int b[][N], int n)
+struct matrix
 {
-    int tmp[N][N];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int k = 0; k < n; k++)
-                tmp[i][j] += a[i][k] * b[k][j];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            c[i][j] = tmp[i][j];
+    ll a[3][3];
+    void init()
+    {
+        memset(a, 0, sizeof(a));
+        for (int i : {1, 2})
+            a[i][i] = 1;
+    }
+    void base()
+    {
+        a[1][1] = 1;
+        a[1][2] = 1;
+        a[2][1] = 1;
+        a[2][2] = 0;
+    }
+};
+
+void deb(matrix &x)
+{
+    for (int i : {1, 2})
+        for (int j : {1, 2})
+        {
+            cout << x.a[i][j] << " \n"[j == 2];
+        }
 }
 
-void fastpow(int a[][N],int n)
+matrix mul(matrix A, matrix B)
 {
-    
+    matrix ans;
+    for (int i : {1, 2})
+    {
+        for (int j : {1, 2})
+        {
+            ans.a[i][j] = 0;
+            for (int k : {1, 2})
+            {
+                ans.a[i][j] %= p;
+                ans.a[i][j] += A.a[i][k] * B.a[k][j] % p;
+            }
+        }
+    }
+    return ans;
+}
+
+matrix fastpow(matrix A, ll b)
+{
+    matrix ans;
+    ans.init();
+    while (b)
+    {
+        if (b & 1)
+        {
+            ans = mul(ans, A);
+        }
+        A = mul(A, A);
+        b >>= 1;
+    }
+    return ans;
 }
 
 void solve()
 {
     cin >> x;
+    matrix base;
+    base.base();
+    matrix ans;
+    ans.a[1][1] = 1;
+    ans.a[1][2] = 0;
+    ans = mul(ans, fastpow(base, x - 1));
+    cout << ans.a[1][1] % p << endl;
 }
 
 int main()
