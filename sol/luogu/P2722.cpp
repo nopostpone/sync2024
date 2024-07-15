e88672456acfe518045401c91637b6d40b407604
Csus4 <2334085989@qq.com>
2024-07-15 14:27:03 +0800

    <new> P2722

---
+++ sol/luogu/P2722.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define enter putchar('\n')
+const int N = 1e3 + 3;
+
+int a[N];
+int n, m;
+int f[N][N];
+
+// 记f[i][j]为考虑前i道菜，有m快钱时的方案数量
+// 如果j == a[i]，那么f[i][j]=f[i-1][j]+1
+// 如果j>a[i]，f[i][j] = f[i-1][j-a[i]]
+
+void solve()
+{
+    cin >> n >> m;
+    int cnt = 0;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (j < a[i])
+                f[i][j] = f[i - 1][j];
+            else if (j == a[i])
+                f[i][j] = f[i - 1][j] + f[i - 1][j - a[i]];
+            else
+                f[i][j] = f[i - 1][j] + 1;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    while (_--)
+        solve();
+    return 0;
+}
