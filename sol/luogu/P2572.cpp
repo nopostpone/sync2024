3a057f6beb245723a079536bc81be9e94e4bf178
Csus4 <2334085989@qq.com>
2024-07-18 21:59:28 +0800

    <update> P2572

--- sol/luogu/P2572.cpp
+++ sol/luogu/P2572.cpp
@@ -28,15 +28,16 @@ void pushup(int pos) {
     s[pos].sum = s[lst].sum + s[rst].sum;
     for (int i : {0, 1}) {
         s[pos].ls[i] = s[lst].ls[i];
-        if ((i == 1 and s[lst].sum == s[lst].r - s[lst].l + 1) or (i == 0 and s[lst].sum == 0))
-            s[pos].ls[i] += s[rst].ls[i];
-
         s[pos].rs[i] = s[rst].rs[i];
-        if ((i == 1 and s[rst].sum == s[rst].r - s[rst].l + 1) or (i == 0 and s[rst].sum == 0))
+        if (s[lst].ls[i] == s[lst].r - s[lst].l + 1) {
+            s[pos].ls[i] += s[rst].ls[i];
+        }
+        if (s[rst].rs[i] == s[rst].r - s[rst].l + 1) {
             s[pos].rs[i] += s[lst].rs[i];
-
+        }
         s[pos].ss[i] = max({s[pos].ls[i], s[pos].rs[i], s[lst].ss[i], s[rst].ss[i], s[lst].rs[i] + s[rst].ls[i]});
     }
+    return;
 }
 
 void build(int l, int r, int pos = 1) {
@@ -60,23 +61,17 @@ void pushdown(int pos) {
     if (~s[pos].tag) {
         int i = s[pos].tag;
         int j = i ^ 1;
-
         s[pos].rev = s[lst].rev = s[rst].rev = 0;
         s[lst].tag = s[rst].tag = i;
-
         s[lst].ls[i] = s[lst].rs[i] = s[lst].ss[i] = s[lst].r - s[lst].l + 1;
         s[rst].rs[i] = s[rst].ls[i] = s[rst].ss[i] = s[rst].r - s[rst].l + 1;
-
         s[lst].ls[j] = s[lst].rs[j] = s[lst].ss[j] = 0;
         s[rst].rs[j] = s[rst].ls[j] = s[rst].ss[j] = 0;
-
         s[lst].sum = i == 1 ? s[lst].r - s[lst].l + 1 : 0;
         s[rst].sum = i == 1 ? s[rst].r - s[rst].l + 1 : 0;
-
         s[pos].tag = -1;
     }
     if (s[pos].rev) {
-        // 左右子树也翻转
         if (~s[lst].tag)
             s[lst].tag ^= 1;
         else
@@ -85,16 +80,18 @@ void pushdown(int pos) {
             s[rst].tag ^= 1;
         else
             s[rst].rev ^= 1;
-
         s[lst].sum = s[lst].r - s[lst].l + 1 - s[lst].sum;
         s[rst].sum = s[rst].r - s[rst].l + 1 - s[rst].sum;
-
+// 喜欢不检查？
+// 喜欢不检查？
+// 喜欢不检查？
+// 喜欢不检查？
         swap(s[lst].ls[0], s[lst].ls[1]);
-        swap(s[rst].ls[0], s[lst].ls[1]);
+        swap(s[rst].ls[0], s[rst].ls[1]);
         swap(s[lst].rs[0], s[lst].rs[1]);
-        swap(s[rst].rs[0], s[lst].rs[1]);
+        swap(s[rst].rs[0], s[rst].rs[1]);
         swap(s[lst].ss[0], s[lst].ss[1]);
-        swap(s[rst].ss[0], s[lst].ss[1]);
+        swap(s[rst].ss[0], s[rst].ss[1]);
 
         s[pos].rev = 0;
     }
@@ -142,57 +139,58 @@ int querysum(int x, int y, int pos = 1) {
     return tmp;
 }
 
-// int querymx(int x, int y, int pos = 1) {
-//     if (x <= s[pos].l and s[pos].r <= y) {
-//         return s[pos].ss[1];
-//     }
-//     pushdown(pos);
-//     int mid = s[pos].l + s[pos].r >> 1;
-//     int tmp = 0;
-//     if (x <= mid)
-//         tmp = max(tmp, querymx(x, y, lst));
-//     if (mid < y)
-//         tmp = max(tmp, querymx(x, y, rst));
-
-//     int tmpr = min(s[lst].rs[1], mid - x + 1), tmpl = min(s[rst].ls[1], y - mid);
-//     tmp = max(tmp, tmpr + tmpl);
-//     return tmp;
-// }
-
-node querymx(int x, int y, int pos = 1) {
+int querymx(int x, int y, int pos = 1) {
+    if (x <= s[pos].l and s[pos].r <= y) {
+        return s[pos].ss[1];
+    }
     pushdown(pos);
-    if (s[pos].l == x and s[pos].r == y) {
-        return s[pos];
-    }
     int mid = s[pos].l + s[pos].r >> 1;
-    if (x > mid) {
-        return querymx(x, y, rst);
-    } else if (y <= mid) {
-        return querymx(x, y, lst);
-    } else {
-        node ret = {};
-        node L = querymx(x, mid, lst);
-        node R = querymx(mid + 1, y, rst);
-        ret.sum = L.sum + R.sum;
-        for (int i : {0, 1}) {
-            ret.ls[i] = L.ls[i];
-            if (i == 1 and L.sum == L.r - L.l + 1) {
-                ret.ls[i] += R.ls[i];
-            } else if (i == 0 and L.sum == 0) {
-                ret.ls[i] += R.ls[i];
-            }
+    int tmp = 0;
+    if (x <= mid)
+        tmp = max(tmp, querymx(x, y, lst));
+    if (mid < y)
+        tmp = max(tmp, querymx(x, y, rst));
 
-            ret.rs[i] = R.rs[i];
-            if (i == 1 and R.sum == R.r - R.l + 1) {
-                ret.rs[i] += L.rs[i];
-            } else if (i == 0 and R.sum == 0) {
-                ret.rs[i] += L.rs[i];
-            }
-            ret.ss[i] = max({L.rs[i] + R.ls[i], L.ss[i], R.ss[i]});
-        }
-        return ret;
-    }
+    int tmpr = min(s[lst].rs[1], mid - x + 1), tmpl = min(s[rst].ls[1], y - mid);
+    tmp = max(tmp, tmpr + tmpl);
+    return tmp;
 }
+
+// 也可以返回一个结构体然后调用ss[1]
+// node querymx(int x, int y, int pos = 1) {
+//     pushdown(pos);
+//     if (s[pos].l == x and s[pos].r == y) {
+//         return s[pos];
+//     }
+//     int mid = s[pos].l + s[pos].r >> 1;
+//     if (x > mid) {
+//         return querymx(x, y, rst);
+//     } else if (y <= mid) {
+//         return querymx(x, y, lst);
+//     } else {
+//         node ret = {};
+//         node L = querymx(x, mid, lst);
+//         node R = querymx(mid + 1, y, rst);
+//         ret.sum = L.sum + R.sum;
+//         for (int i : {0, 1}) {
+//             ret.ls[i] = L.ls[i];
+//             if (i == 1 and L.sum == L.r - L.l + 1) {
+//                 ret.ls[i] += R.ls[i];
+//             } else if (i == 0 and L.sum == 0) {
+//                 ret.ls[i] += R.ls[i];
+//             }
+
+//             ret.rs[i] = R.rs[i];
+//             if (i == 1 and R.sum == R.r - R.l + 1) {
+//                 ret.rs[i] += L.rs[i];
+//             } else if (i == 0 and R.sum == 0) {
+//                 ret.rs[i] += L.rs[i];
+//             }
+//             ret.ss[i] = max({L.rs[i] + R.ls[i], L.ss[i], R.ss[i]});
+//         }
+//         return ret;
+//     }
+// }
 
 void solve() {
     cin >> n >> m;
@@ -209,7 +207,7 @@ void solve() {
         } else if (opt == 3) {
             res.push_back(querysum(l, r));
         } else if (opt == 4) {
-            res.push_back(querymx(l, r).ss[1]);
+            res.push_back(querymx(l, r));
         }
     }
     for (auto i : res) {
