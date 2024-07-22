f3a2a96c2ef4e03e4b225270ecec778dcef0f8f7
Csus4 <2334085989@qq.com>
2024-07-23 07:55:10 +0800

    <new> tictactoe

---
+++ demo/newTictactoe.cpp
@@ -0,0 +1,79 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+struct Pos {
+    std::vector<int> dx, dy;
+    Pos() {}
+    ~Pos() {}
+};
+
+struct Blocks {
+
+    Blocks() {}
+    ~Blocks() {}
+};
+
+template <class Blocks, class Pos>
+struct Scene {
+    typedef Blocks blks;
+    std::vector<std::vector<char>> scene;
+
+    Scene() {
+        std::function<void(int, int, int)> init {
+
+        }
+        init();
+    }
+    ~Scene() {}
+};
+
+int main(void) {
+
+    return 0;
+}
