4d88f30c992fb9b70a6450075a3222d8278b4634
Csus4 <2334085989@qq.com>
2024-07-26 07:13:49 +0800

    <update> CF765F

--- sol/CF/765F.cpp
+++ sol/CF/765F.cpp
@@ -3,30 +3,31 @@
 // 维护后缀最小值，从右往左更新，每个点被修改的次数不超过 loga 次
 // 复杂度 O ( nlognloga )
 #include <bits/stdc++.h>
-#define lst pos << 1
-#define rst pos << 1 | 1
 #define endl '\n'
 using namespace std;
 using ll = long long;
 
-const int N = 3e5 + 3;
+constexpr int N = 3e5 + 3;
+constexpr int inf = 2e9;
 
-vector<ll> a;
+vector<int> a;
 int n, m, lim;
 
 struct node {
     int l, r;
-    int mnv;
-    // 存管辖区间所有数，二分查找
+    int v;
     vector<int> lis;
 } s[N << 2];
 
-inline void pushup(int pos) {
-    s[pos].mnv = min(s[lst].mnv, s[rst].mnv);
+#define lst pos << 1
+#define rst pos << 1 | 1
+
+inline void pullup(int pos) {
+    s[pos].v = min(s[lst].v, s[rst].v);
 }
 
 void build(int l, int r, int pos = 1) {
-    s[pos].l = l, s[pos].r = r, s[pos].mnv = 0x3f3f3f3f;
+    s[pos].l = l, s[pos].r = r, s[pos].v = inf;
     for (int i = l; i <= r; i++) {
         s[pos].lis.push_back(a[i]);
     }
@@ -37,7 +38,7 @@ void build(int l, int r, int pos = 1) {
     int m = (l + r) >> 1;
     build(l, m, lst);
     build(m + 1, r, rst);
-    pushup(pos);
+    pullup(pos);
     return;
 }
 
@@ -45,17 +46,17 @@ void upd(int x, int R, int pos = 1) {
     if (1 <= s[pos].l and s[pos].r <= R) {
         auto it = lower_bound(s[pos].lis.begin(), s[pos].lis.end(), x);
         if (it != s[pos].lis.end()) {
-            s[pos].mnv = min(s[pos].mnv, *it - x);
+            s[pos].v = min(s[pos].v, *it - x);
         }
         if (it != s[pos].lis.begin()) {
-            s[pos].mnv = min(s[pos].mnv, x - *(it - 1));
+            s[pos].v = min(s[pos].v, x - *(it - 1));
         }
-        if (s[pos].mnv >= lim) {
+        if (s[pos].v >= lim) {
             return;
         }
     }
     if (s[pos].l == s[pos].r) {
-        lim = min(lim, s[pos].mnv);
+        lim = min(lim, s[pos].v);
         return;
     }
     int mid = s[pos].l + s[pos].r >> 1;
@@ -63,13 +64,13 @@ void upd(int x, int R, int pos = 1) {
         upd(x, R, rst);
     }
     upd(x, R, lst);
-    pushup(pos);
-    lim = min(lim, s[pos].mnv);
+    pullup(pos);
+    lim = min(lim, s[pos].v);
 }
 
 int query(int L, int pos = 1) {
     if (L <= s[pos].l and s[pos].r <= n) {
-        return s[pos].mnv;
+        return s[pos].v;
     }
     int tmp = 0x3f3f3f3f;
     int mid = s[pos].l + s[pos].r >> 1;
@@ -87,32 +88,31 @@ struct Q {
     }
 };
 
-vector<Q> qq;
-vector<int> res;
-
 void solve() {
     cin >> n;
     a.resize(n + 1);
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
+    build(1, n);
+
     cin >> m;
-    qq.resize(m + 1);
-    res.resize(m + 1);
+    vector<Q> qq(m + 1);
+    vector<int> res(m + 1);
     for (int i = 1; i <= m; i++) {
         cin >> qq[i].l >> qq[i].r;
         qq[i].id = i;
     }
+
     // 离线，排序
     sort(qq.begin() + 1, qq.end());
-    build(1, n);
     int loc = 1;
     // 找到一个右端点 > 1 的询问
     while (qq[loc].r <= 1) {
         loc++;
     }
     for (int i = 2; i <= n; i++) {
-        lim = 0x3f3f3f3f;
+        lim = inf;
         upd(a[i], i - 1);
         while (qq[loc].r == i) {
             res[qq[loc].id] = query(qq[loc].l);
