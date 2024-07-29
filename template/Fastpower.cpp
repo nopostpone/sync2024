4ae43db4497d62da18cf37c6b608bbcfa762f85c
Csus4 <2334085989@qq.com>
2024-07-29 19:10:58 +0800

    <new> fastpower template

---
+++ template/Fastpower.cpp
@@ -0,0 +1,10 @@
+template <class T>
+T power(int a, T b, T p) {
+    T res = 1;
+    for (; b; b /= 2, a = 1LL * a * a % p) {
+        if (b % 2) {
+            res = 1LL * res * a % p;
+        }
+    }
+    return res;
+}
