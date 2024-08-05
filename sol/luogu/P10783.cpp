529ef9868a51515e3546fdfa215a360e480353cd
Csus4 <2334085989@qq.com>
2024-08-05 09:47:17 +0800

    <update> P10783

--- sol/luogu/P10783.cpp
+++ sol/luogu/P10783.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
 using ll = long long;
 
@@ -8,19 +9,35 @@ void solve() {
     vector f(n, vector<ll>(1 << n));
     for (int i = 1; i < n; i++)
         cin >> f[0][i];
+    vector<int> dep(1 << n);
+    for (int i = 1; i < dep.size(); i++) {
+        dep[i] = log2(i);
+    }
 
     function<void(int)> dfs = [&](int x) {
-        if (x * 2 > (1 << (n - 1))) {
-            for ()
+        if (x * 2 >= (1 << n)) {
+            for (int i = 1; i <= n; i++) {
+                f[i][x] = f[0][x];
+            }
             return;
         }
         dfs(2 * x);
         dfs(2 * x + 1);
         for (int i = 1; i <= n; i++) {
-            f[i][x] += f[i - 1][x * 2] + f[i - 1][x * 2 + 1];
+            f[i][x] = f[0][x] + f[i - 1][x * 2] + f[i - 1][x * 2 + 1];
         }
     };
     dfs(1);
+
+    vector<ll> res;
+    while (m--) {
+        int x, y, k;
+        cin >> x >> y >> k;
+        if (x < y)
+            swap(x, y);
+    }
+    for (auto i : res)
+        cout << i << endl;
 }
 
 int main() {
