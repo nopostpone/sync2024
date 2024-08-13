1ae253e3c24950fbf38369ffe42b1c230b636c6d
Csus4 <2334085989@qq.com>
2024-08-13 21:40:35 +0800

    <new> P2014(TODO)

---
+++ sol/luogu/P2014.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int n, m;
+    cin >> n >> m;   
+
+    return 0;
+}
