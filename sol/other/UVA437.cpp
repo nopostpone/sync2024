b775892e47a2e04de13ec6a589231d2b9d90fbfa
Csus4 <2334085989@qq.com>
2024-08-13 20:56:21 +0800

    <new> UVA437(todo)

---
+++ sol/other/UVA437.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+using ll = long long;
+
+void answer(int a, ll b) {
+    
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    if (!n) {
+        exit(0);
+    }
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    while (true) {
+        solve();
+    }
+    return 0;
+}
