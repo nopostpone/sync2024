76a31f9c90ea100cf8ba92a615e6c10a5549fe41
Csus4 <2334085989@qq.com>
2024-08-15 19:35:54 +0800

    <update> abc366e

--- sol/Atcoder/abc366_e.cpp
+++ sol/Atcoder/abc366_e.cpp
@@ -3,62 +3,51 @@
 using namespace std;
 using ll = long long;
 
-struct D {
-    int n;
-    ll d;
-    vector<int> a;
-    vector<int> t;
-    vector<ll> f;
-
-    D() {}
-
-    D(const vector<int> &_v) {
-        init(_v);
-        work();
-    }
-
-    void init(const vector<int> &_v) {
-        n = _v.size();
-        a = _v;
-        t.resize(2e6 + 1);
-        f.resize(2e6 + 1);
-        d = 0;
-    }
-
-    void work() {
-        for (auto &i : a) {
-            d += i;
-        }
-        sort(a.begin(), a.end());
-        int p = 0;
-        for (int i = -1e6; i <= 1e6; i++) {
-            while (a[p] == i and p < n - 1) {
-                p++;
-            }
-            t[i + 1 * 1e6] = p;
-        }
-        for (int i = -1e6; i <= 1e6; i++) {
-            
-        }
-    }
-};
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-
-    int n, m;
-    cin >> n >> m;
-
-    D X, Y;
-
-    vector<int> tx(n), ty(n);
-    for (int i = 0; i < n; i++) {
-        cin >> tx[i] >> ty[i];
-    }
-    X.init(tx);
-    Y.init(ty);
-
     
+    int n, D;
+    cin >> n >> D;
 
+    vector<int> x(n), y(n);
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    sort(x.begin(), x.end());
+    sort(y.begin(), y.end());
+
+    vector<ll> sx(n + 1), sy(n + 1);
+    for (int i = 1; i <= n; i++) {
+        sx[i] = 1ll * x[i - 1] + sx[i - 1];
+        sy[i] = 1ll * y[i - 1] + sy[i - 1];
+    }
+
+    vector<ll> cnt(D + 1);
+
+    for (int i = -2e6; i <= 2e6; i++) {
+        int t = upper_bound(y.begin(), y.end(), i) - y.begin();
+        ll dist = 1ll * i * t - sy[t] + sy[n] - sy[t] - 1ll * (n - t) * i;
+        if (dist > D)
+            continue;
+        cnt[dist]++;
+    }
+
+    for (int i = 1; i <= D; i++) {
+        cnt[i] += cnt[i - 1];
+    }
+
+    ll res = 0;
+    for (int i = -2e6; i <= 2e6; i++) {
+        int t = upper_bound(x.begin(), x.end(), i) - x.begin();
+        ll dist = 1ll * i * t - sx[t] + sx[n] - sx[t] - 1ll * (n - t) * i;
+        if (dist > D)
+            continue;
+        assert(dist >= 0);
+        res += cnt[D - dist];
+    }
+
+    cout << res << endl;
+    
     return 0;
 }
