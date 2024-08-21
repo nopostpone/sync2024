aa24e38cc7a923883b2866110207194fa2339135
Csus4 <2334085989@qq.com>
2024-08-22 00:07:09 +0800

    <new> abc365e

---
+++ sol/Atcoder/abc365_e.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    
+
+    return 0;
+}
