aaa8c7a0917374518c26a335ec831f16555b9c7d
Csus4 <2334085989@qq.com>
2024-08-21 23:58:42 +0800

    <update> abc367f

--- sol/Atcoder/abc367_f.cpp
+++ sol/Atcoder/abc367_f.cpp
@@ -3,46 +3,40 @@
 using namespace std;
 using ll = long long;
 
+mt19937_64 rng{chrono::steady_clock::now().time_since_epoch().count()};
+constexpr int P = 1e9 + 7;
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-    
-    int n;
-    ll k;
-    cin >> n >> k;
 
-    vector<int> x(n), a(n);
-    for (int i = 0; i < n; i++)
-        cin >> x[i];
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n), b(n);
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    
-    vector f(n, vector<int>(64));
-    for (int i = 0; i < n; i++) {
-        f[i][0] = x[i] - 1;
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+
+    vector<ll> hs(n);
+    for (auto &i : hs) {
+        i = rng() % P;
     }
 
-    for (int j = 1; j < 64; j++) {
-        for (int i = 0; i < n; i++) {
-            f[i][j] = f[f[i][j - 1]][j - 1];
-        }
+    vector<ll> prea(n + 1), preb(n + 1);
+    for (int i = 1; i <= n; i++) {
+        prea[i] = prea[i - 1] + hs[a[i - 1]];
+        preb[i] = preb[i - 1] + hs[b[i - 1]];
     }
 
-    vector<int> res(n);
-    iota(res.begin(), res.end(), 0);
-
-    int t = 0;
-    while (k) {
-        if (k & 1) {
-            for (int i = 0; i < n; i++) {
-                res[i] = f[res[i]][t];
-            }
-        }
-        t++;
-        k /= 2;
+    vector<bool> res;
+    while (q--) {
+        int l, r, L, R;
+        cin >> l >> r >> L >> R;
+        res.push_back((prea[r] - prea[l - 1] == preb[R] - preb[L - 1]) and (r - l == R - L));
     }
-
     for (auto i : res) {
-        cout << a[i] << " ";
+        cout << (i ? "Yes" : "No") << endl;
     }
 
     return 0;
