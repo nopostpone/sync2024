ec97a43321f7a9caf5e66bf94c68440f6e193196
Csus4 <2334085989@qq.com>
2024-08-24 02:11:05 +0800

    <update> nc85598E

--- sol/other/nc85598_e.cpp
+++ sol/other/nc85598_e.cpp
@@ -16,14 +16,30 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> a[i];
     
-    vector p(n + 1, vector<int>(64));
-    for (int i = 1; i <= n; i++) {
-        for (int j = k1; j < 64; j++) {
-            p[i][j] = p[i - 1][j] + ((a[i - 1] >> j) & 1);
+    auto work = [&](int x) {
+        ll ans = 0, cnt = 1;
+        for (int i = 1; i < n; i++) {
+            bool ok = true;
+            for (int j = x; j < 64; j++) {
+                bool u = a[i] >> j & 1;
+                bool v = a[i - 1] >> j & 1;
+                if (u != v) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                cnt++;
+            } else {
+                ans += cnt * (cnt + 1) / 2;
+                cnt = 1;
+            }
         }
-    }
+        ans += cnt * (cnt + 1) / 2;
+        return ans;
+    };
 
-    
+    cout << work(k2) - work(k1) << endl;
     
     return 0;
 }
