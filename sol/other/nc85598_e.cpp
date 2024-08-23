52fd4a888f5c0533a1e31228e3facca7f3dac12d
Csus4 <2334085989@qq.com>
2024-08-23 23:55:52 +0800

    <new> nc855989E (todo)

---
+++ sol/other/nc85598_e.cpp
@@ -0,0 +1,13 @@
+// https://ac.nowcoder.com/acm/contest/85598/E
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    
+    
+    return 0;
+}
