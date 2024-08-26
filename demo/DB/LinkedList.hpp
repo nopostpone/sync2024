2d031b2a83ce35b5c82908a6375230acb09aaab7
Csus4 <2334085989@qq.com>
2024-08-26 16:17:10 +0800

    <update> db : LinkedList (todo)

--- demo/DB/LinkedList.hpp
+++ demo/DB/LinkedList.hpp
@@ -1,3 +1,5 @@
+// TODO
+
 template <class T>
 struct Node {
     T val;
@@ -6,7 +8,7 @@ struct Node {
 
 template <class T>
 struct LinkedList {
-    int N;
+    int siz;
 
     Node<T> *head;
     Node<T> *tail;
@@ -22,18 +24,21 @@ struct LinkedList {
     void init() {
         head = nullptr;
         tail = nullptr;
-        N = 0;
+        siz = 0;
     }
 
     void init(const T _init[], int _siz) {
-        N = _siz;
+        head = nullptr;
+        tail = nullptr;
+        siz = _siz;
         
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < siz; i++) {
             Node<T> *t = new Node<T>;
             t->val = _init[i];
             t->next = nullptr;
+            
             if (tail == nullptr) {
-
+                head = tail = t;
             } else {
                 tail->next = t;
                 tail = t;
@@ -41,27 +46,53 @@ struct LinkedList {
         }
     }
 
-    void insert(int _x, const T &_val) {
-        assert(0 <= x && x < N);
+    bool insert(int _x, const T &_val) {
+        if (_x < 0 || _x >= siz) {
+            return false;
+        }
 
-        for (int i = 0; i < N; i++) {
+        Node<T> *t = new Node<T>;
+        for (int i = 0; i < siz; i++) {
 
         }
-        N++;
+        siz++;
     }
 
-    constexpr T operator[](int x) {
-        assert(0 <= x && x < N);
+    void push_back(const T &_val) {
+        insert(siz, _val);
+    }
 
-        Node<T> t = head;
-        for (int i = 0; i < x; i++) {
+    void push_front(const T _val) {
+        insert(0, _val);
+    }
+
+    int size() { return siz; }
+    
+    void clear() {
+        siz = 0;
+        head = tail = nullptr;
+    }
+
+    constexpr T operator[](unsigned x) {
+        Node<T> *t = head;
+        for (int i = 0; i < x && t != nullptr; i++) {
             t = t->next;       
         }
         return t->val;
     }
 
     constexpr void operator+=(const LinkedList<T> &_L) {
-        
+        tail->next = _L.head;
+        siz += _L.siz;
     }
-
 };
+
+template<class T>
+constexpr LinkedList<T> operator+(const LinkedList<T> &A, const LinkedList<T> &B) {
+    LinkedList<T> C;
+    C.head = A.head;
+    C.tail = B.tail;
+    A.tail->next = B.head;
+    C.siz = A.siz + B.siz;
+    return C;
+}
--- demo/DB/LinkedListTest.cpp
+++ demo/DB/LinkedListTest.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 int main() {
 
-    LinkedList<int> l;
+    int a[] = {1, 2, 3, 4, 5};
+
+    LinkedList<int> l(a, 5);
+
+    l.push_back(1);
+
+    cerr << l[0];
+
+    
 
     return 0;
 }
