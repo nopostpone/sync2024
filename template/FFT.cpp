2daf3217ec1548209b5552df1cbecd9f882258a9
Csus4 <2334085989@qq.com>
2024-08-28 19:05:22 +0800

    <update> fft template

--- template/FFT.cpp
+++ template/FFT.cpp
@@ -1,88 +1,105 @@
-constexpr int P = 998244353;
+#define x first
+#define y second
 
-int power(int a, int b) {
-    int res = 1;
-    for (; b; b /= 2, a = 1LL * a * a % P) {
-        if (b % 2) {
-            res = 1LL * res * a % P;
-        }
+template <class T>
+struct Complex : public std::pair<T, T> {
+    constexpr Complex(T x = T(), T y = T()) : std::pair<T, T>(x, y) {}
+
+    constexpr Complex operator+(const Complex &a) const {
+        return Complex(this->x + a.x, this->y + a.y);
     }
-    return res;
+    constexpr Complex operator-(const Complex &a) const {
+        return Complex(this->x - a.x, this->y - a.y);
+    }
+    constexpr Complex operator*(const Complex &a) const {
+        return Complex(this->x * a.x - this->y * a.y, this->x * a.y + this->y * a.x);
+    }
+};
+using ld = double;
+using Comp = Complex<ld>;
+
+constexpr ld pi = acosl(-1);
+
+std::vector<Comp> w[2];
+std::vector<int> r;
+
+constexpr void init(int _log) {
+    if (r.size() == (1 << _log))
+        return;
+
+    int n = 1 << _log;
+    r.assign(n, 0);
+    for (int i = 1; i < n; i++)
+        r[i] = (r[i >> 1] >> 1) | ((i & 1) << (_log - 1));
+
+    w[0].assign(n, Comp());
+    w[1].assign(n, Comp());
+    for (int i = 0; i < n; i++) {
+        auto th = 1.L * pi * i / n;
+        w[0][i] = Comp(cosl(th), sinl(th));
+        w[1][i] = Comp(cosl(th), -sinl(th));
+    }
 }
 
-std::vector<int> rev, roots {0, 1};
-
-void dft(std::vector<int> &a) {
+void fft(std::vector<Comp> &a, int op) {
     int n = a.size();
-    if (int(rev.size()) != n) {
-        int k = __builtin_ctz(n) - 1;
-        rev.resize(n);
-        for (int i = 0; i < n; i++) {
-            rev[i] = rev[i >> 1] >> 1 | (i & 1) << k;
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        if (rev[i] < i) {
-            std::swap(a[i], a[rev[i]]);
-        }
-    }
-    if (roots.size() < n) {
-        int k = __builtin_ctz(roots.size());
-        roots.resize(n);
-        while ((1 << k) < n) {
-            int e = power(31, 1 << (__builtin_ctz(P - 1) - k - 1));
-            for (int i = 1 << (k - 1); i < (1 << k); i++) {
-                roots[2 * i] = roots[i];
-                roots[2 * i + 1] = 1LL * roots[i] * e % P;
-            }
-            k++;
-        }
-    }
-
-    for (int k = 1; k < n; k *= 2) {
-        for (int i = 0; i < n; i += 2 * k) {
-            for (int j = 0; j < k; j++) {
-                int u = a[i + j];
-                int v = 1LL * a[i + j + k] * roots[k + j] % P;
-                a[i + j] = (u + v) % P;
-                a[i + j + k] = (u - v) % P;
+    init(std::__lg(n));
+    for (int i = 0; i < n; i++)
+        if (i < r[i])
+            std::swap(a[i], a[r[i]]);
+    for (int mid = 1; mid < n; mid <<= 1) {
+        const int d = n / mid;
+        for (int R = mid << 1, j = 0; j < n; j += R) {
+            for (int k = 0; k < mid; k++) {
+                Comp x = a[j + k], y = w[op][d * k] * a[j + mid + k];
+                a[j + k] = x + y;
+                a[j + mid + k] = x - y;
             }
         }
     }
 }
 
-void idft(std::vector<int> &a) {
-    int n = a.size();
-    std::reverse(a.begin() + 1, a.end());
-    dft(a);
-    int inv = (1 - P) / n;
-    for (int i = 0; i < n; i++) {
-        a[i] = 1LL * a[i] * inv % P;
-    }
-}
+template <class T>
+struct Polynomial : public std::vector<T> {
+    explicit constexpr Polynomial(int n = 0, T v = T()) : std::vector<T>(n, v) {}
+    explicit constexpr Polynomial(const std::vector<T> &a) : std::vector<T>(a) {}
+    constexpr Polynomial(const std::initializer_list<T> &a) : std::vector<T>(a) {}
 
-std::vector<int> mul(std::vector<int> a, std::vector<int> b) {
-    int n = 1, tot = a.size() + b.size() - 1;
-    while (n < tot) {
-        n *= 2;
-    }
-    if (tot < 128) {
-        std::vector<int> c(a.size() + b.size() - 1);
-        for (int i = 0; i < a.size(); i++) {
-            for (int j = 0; j < b.size(); j++) {
-                c[i + j] = (c[i + j] + 1LL * a[i] * b[j]) % P;
-            }
+    template <class IT, class = std::_RequireInputIter<IT>>
+    explicit constexpr Polynomial(IT first, IT last) : std::vector<T>(first, last) {}
+
+    constexpr friend Polynomial operator*(const Polynomial &a, const Polynomial &b) {
+        if (a.size() == 0 or b.size() == 0)
+            return Polynomial();
+        int n = a.size() + b.size() - 1;
+        int _log = std::__lg(2 * n - 1);
+        int s = 1 << _log;
+        if (std::min(a.size(), b.size()) < 128) {
+            Polynomial res(n);
+            for (int i = 0; i < a.size(); i++)
+                for (int j = 0; j < b.size(); j++)
+                    res[i + j] += a[i] * b[j];
+            return res;
         }
-        return c;
+
+        std::vector<Comp> p(s), q(s);
+        for (int i = 0; i < a.size(); i++)
+            p[i].x = a[i];
+        for (int i = 0; i < b.size(); i++)
+            q[i].x = b[i];
+
+        fft(p, 0), fft(q, 0);
+        for (int i = 0; i < s; i++)
+            p[i] = p[i] * q[i];
+        fft(p, 1);
+
+        Polynomial res(n);
+        for (int i = 0; i < n; i++)
+            res[i] = p[i].x / s; // 默认浮点数
+        return res;
     }
-    a.resize(n);
-    b.resize(n);
-    dft(a);
-    dft(b);
-    for (int i = 0; i < n; i++) {
-        a[i] = 1LL * a[i] * b[i] % P;
-    }
-    idft(a);
-    a.resize(tot);
-    return a;
-}
+};
+
+using Poly = Polynomial<ld>;
+#undef x
+#undef y
