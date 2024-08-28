87b706e86a4e63218d86affa20fdd7870d4d8136
Csus4 <2334085989@qq.com>
2024-08-28 14:17:31 +0800

    <update> P1786 done

--- sol/luogu/P1786.cpp
+++ sol/luogu/P1786.cpp
@@ -1,97 +1,60 @@
 #include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
-#define endl '\n'
+using ll = long long;
 
-const int N = 110;
+constexpr int inf = 2e9;
 
-struct node
-{
-    string name;
-    int prof, idx, lvl, val;
-    bool operator<(const node &o)
-    {
-        return prof == o.prof ? lvl == o.lvl ? idx < o.idx : lvl > o.lvl : prof < o.prof;
-        // return prof == o.prof ? idx < o.idx : prof < o.prof;
-    }
-} a[N];
+struct Node {
+    string name, job;
+    int id, money, lvl;
+};
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
 
-int strtoint(string &o)
-{
-    if (o == "BangZhu")
-        return 1;
-    else if (o == "FuBangZhu")
-        return 2;
-    else if (o == "HuFa")
-        return 3;
-    else if (o == "ZhangLao")
-        return 4;
-    else if (o == "TangZhu")
-        return 5;
-    else if (o == "JingYing")
-        return 6;
-    else
-        return 7;
-}
-
-string inttostr(int &x)
-{
-    if (x == 1)
-        return "BangZhu";
-    else if (x == 2)
-        return "FuBangZhu";
-    else if (x == 3)
-        return "HuFa";
-    else if (x == 4)
-        return "ZhangLao";
-    else if (x == 5)
-        return "TangZhu";
-    else if (x == 6)
-        return "JingYing";
-    else
-        return "BangZhong";
-}
-
-int profmax[] = {0, 0, 0, 2, 4, 7, 25, 0x3f3f3f3f};
-
-void solve()
-{
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        string name,
-            oriprof;
-        int val, lvl;
-        cin >> name >> oriprof >> val >> lvl;
-        a[i] = node{name, strtoint(oriprof), i, lvl, val};
-    }
-    sort(a + 1, a + 1 + n, [](node &x, node &y)
-         { return x.val > y.val; });
-    for (int i = 1; i <= n; i++)
-    {
-        if (a[i].prof < 3)
-            continue;
-        for (int j = 3; j <= 7; j++)
-            if (profmax[j])
-            {
-                a[i].prof = j;
-                profmax[j]--;
-                break;
-            }
-    }
-    sort(a + 1, a + n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        cout << a[i].name << ' ' << inttostr(a[i].prof) << ' ' << a[i].lvl << endl;
-    }
-    return;
-}
+    vector<Node> a(n);
 
-int main()
-{
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int _ = 1;
-    while (_--)
-        solve();
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].name >> a[i].job >> a[i].money >> a[i].lvl;
+        a[i].id = i;
+    }
+
+    map<string, int> f;
+    f["BangZhu"] = -2;
+    f["FuBangZhu"] = -1;
+    f["HuFa"] = 0;
+    f["ZhangLao"] = 1;
+    f["TangZhu"] = 2;
+    f["JingYing"] = 3;
+    f["BangZhong"] = 4;
+
+    vector<string> g(5);
+    for (auto &&[str, i] : f) {
+        if (i >= 0)
+            g[i] = str;
+    }
+    sort(a.begin(), a.end(), [](Node &x, Node &y) { return x.money == y.money ? x.id < y.id : x.money > y.money;});
+
+    int cnt[] = {2, 4, 7, 25, inf};
+    int pos = 0;
+    for (auto &I : a) {
+        int JOB = f[I.job];
+        if (JOB < 0) {
+            continue;
+        }
+        I.job = g[pos];
+        if (--cnt[pos] == 0) {
+            pos++;
+        }
+    }
+
+    sort(a.begin(), a.end(), [&](auto x, auto y) { return x.job == y.job ? x.lvl == y.lvl ? x.id < y.id : x.lvl > y.lvl : f[x.job] < f[y.job];});
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i].name << " " << a[i].job << " " << a[i].lvl << endl;
+    }
+
     return 0;
 }
