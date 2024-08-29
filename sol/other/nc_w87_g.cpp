bafda18f333d2cd3eafabcb720c3a083c3f59280
Csus4 <2334085989@qq.com>
2024-08-30 02:37:14 +0800

    <new> nc_w87_g (todo)

---
+++ sol/other/nc_w87_g.cpp
@@ -0,0 +1,17 @@
+// https://ac.nowcoder.com/acm/contest/73854/G
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+using ll = long long;
+
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+
+    
+
+    return 0;
+}
