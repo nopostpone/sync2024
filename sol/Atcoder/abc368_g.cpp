64722a9b3dc55d2321a735ee5cd79d5f76aed8a0
Csus4 <2334085989@qq.com>
2024-09-06 00:18:24 +0800

    <update> abc368_g (TODO)

--- sol/Atcoder/abc368_g.cpp
+++ sol/Atcoder/abc368_g.cpp
@@ -3,10 +3,40 @@
 using namespace std;
 using ll = long long;
 
+
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     
-    
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+
+
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int op;
+        cin >> op;
+        if (op == 3) {
+            int l, r;
+            cin >> l >> r;
+            l--, r--;
+        } else {
+            int i, x;
+            cin >> i >> x;
+            i--;
+        }
+    }
     
     return 0;
 }
