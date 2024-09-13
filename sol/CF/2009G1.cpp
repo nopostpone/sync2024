b29c35e68f955d106f03a03d9723eca000373b4c
Csus4 <2334085989@qq.com>
2024-09-14 01:12:09 +0800

    <new> CF2009G1 (todo)

---
+++ sol/CF/2009G1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n, k, q;
+    cin >> n >> k >> q;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int T;
+    for (cin >> T; T--;) {
+        solve();
+    }
+
+    return 0;
+}
