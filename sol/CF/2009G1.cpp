ced2e48ed6d4e117fc78bbd7a2b691b8283e94f5
Csus4 <2334085989@qq.com>
2024-09-14 12:19:39 +0800

    <update> CF2009G1

--- sol/CF/2009G1.cpp
+++ sol/CF/2009G1.cpp
@@ -10,6 +10,36 @@ void solve() {
     for (int i = 0; i < n; i++)
         cin >> a[i];
     
+    map<int, int> f;
+    multiset<int> s;
+    
+    for (int i = 0; i < n; i++) {
+        s.insert(0);
+    }
+    for (int i = 0; i < k - 1; i++) {
+        s.extract(f[a[i] - i]);
+        f[a[i] - i]++;
+        s.insert(f[a[i] - i]);
+    }
+    
+    vector<int> ans(n);
+    for (int i = k - 1; i < n; i++) {
+        s.extract(f[a[i] - i]);
+        f[a[i] - i]++;
+        s.insert(f[a[i] - i]);
+        
+        int p = i - k + 1;
+        ans[p] = k - *s.rbegin();
+        s.extract(f[a[p] - p]);
+        f[a[p] - p]--;
+        s.insert(f[a[p] - p]);
+    }
+    
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << ans[l - 1] << endl;
+    }
 }
 
 int main() {
