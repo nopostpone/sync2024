8b42ebc8a2f36787ad07d6687590db1a4d177d9a
Csus4 <2334085989@qq.com>
2024-09-16 04:41:42 +0800

    <update> LinkedList (todo)

--- demo/DS/LinkedList.hpp
+++ demo/DS/LinkedList.hpp
@@ -2,99 +2,46 @@
 
 template <class T>
 struct Node {
-    T val;
-    Node *next; 
+    T val = {};
+    Node *prev = nullptr;
+    Node *next = nullptr;
 };
 
-template <class T>
 struct LinkedList {
-    int siz;
+    size_t siz;
 
-    Node<T> *head;
-    Node<T> *tail;
+    Node *head;
+    Node *tail;
 
     LinkedList() {
         init();
     }
     
-    LinkedList(const T _init[], const int _siz) {
-        init(_init, _siz);
-    }
-    
     ~LinkedList() {}
 
     void init() {
-        head = nullptr;
-        tail = nullptr;
+        head = new Node;
+        tail = new Node;
+        head->next = tail;
+        tail->prev = head;
         siz = 0;
     }
 
-    void init(const T _init[], int _siz) {
-        head = nullptr;
-        tail = nullptr;
-        siz = _siz;
-        
-        for (int i = 0; i < siz; i++) {
-            Node<T> *t = new Node<T>;
-            t->val = _init[i];
-            t->next = nullptr;
-            
-            if (tail == nullptr) {
-                head = tail = t;
-            } else {
-                tail->next = t;
-                tail = t;
-            }
-        }
+    size_t size() {
+        return siz;
     }
 
-    bool insert(int _x, const T &_val) {
-        if (_x < 0 || _x >= siz) {
-            return false;
-        }
+    template <class T>
+    bool insert(Node *_p, T _val) {
+        Node q = new Node();
 
-        Node<T> *t = new Node<T>;
-        for (int i = 0; i < siz; i++) {
-            t = t->next;
-        }
-        siz++;
+        
     }
 
     void push_back(const T &_val) {
-        insert(siz, _val);
     }
 
     void push_front(const T _val) {
-        insert(0, _val);
     }
 
-    int size() { return siz; }
-    
-    void clear() {
-        siz = 0;
-        head = tail = nullptr;
-    }
-
-    constexpr T operator[](unsigned x) {
-        Node<T> *t = head;
-        for (int i = 0; i < x && t != nullptr; i++) {
-            t = t->next;       
-        }
-        return t->val;
-    }
-
-    constexpr void operator+=(const LinkedList<T> &_L) {
-        tail->next = _L.head;
-        siz += _L.siz;
-    }
 };
-
-template<class T>
-constexpr LinkedList<T> operator+(const LinkedList<T> &A, const LinkedList<T> &B) {
-    LinkedList<T> C;
-    C.head = A.head;
-    C.tail = B.tail;
-    A.tail->next = B.head;
-    C.siz = A.siz + B.siz;
-    return C;
-}
