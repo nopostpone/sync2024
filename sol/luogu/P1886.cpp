979b1ef6f46511ca7d0c89b85b1ce2b157161646
Csus4 <2334085989@qq.com>
2024-09-19 00:47:09 +0800

    <update> P1886

--- sol/luogu/P1886.cpp
+++ sol/luogu/P1886.cpp
@@ -1,52 +1,50 @@
 // https://www.luogu.com.cn/problem/P1886
 // 单调队列
+// 24.9.19 upd: 更整洁
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define endl '\n'
-#define enter putchar('\n')
-const int N = 3e6 + 3;
 
-ll n, k;
-ll a[N];
-deque<ll> da, xiao;
-vector<vector<ll>> res;
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
 
-void solve()
-{
-    cin >> n >> k;
-    vector<ll> tmp(n);
-    res.resize(2, tmp);
-    for (int i = 1; i <= n; i++)
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
-    for (int i = 1; i <= n; i++)
-    {
-        while (!da.empty() and a[i] >= a[da.back()])
-            da.pop_back();
-        while (!xiao.empty() and a[i] <= a[xiao.back()])
-            xiao.pop_back();
-        da.push_back(i);
-        xiao.push_back(i);
-        while (i - k >= da.front())
-            da.pop_front();
-        while (i - k >= xiao.front())
-            xiao.pop_front();
-        if (i >= k)
-        {
-            res[0][i - k] = a[da.front()];
-            res[1][i - k] = a[xiao.front()];
+    }
+
+    deque<int> dq;
+    for (int i = 0; i < n; i++) {
+        if (dq.front() == i - m) {
+            dq.pop_front();
+        }
+        while (not dq.empty() and a[dq.back()] >= a[i]) {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+
+        if (i >= m - 1) {
+            cout << a[dq.front()] << " \n"[i == n - 1];
         }
     }
-    for (int i : {1, 0})
-        for (int j = 0; j < n - k + 1; j++)
-            cout << res[i][j] << " \n"[j == n - k];
-}
 
-int main()
-{
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int _ = 1;
-    while (_--)
-        solve();
+    dq.clear();
+    for (int i = 0; i < n; i++) {
+        if (dq.front() == i - m) {
+            dq.pop_front();
+        }
+        while (not dq.empty() and a[dq.back()] <= a[i]) {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+
+        if (i >= m - 1) {
+            cout << a[dq.front()] << " \n"[i == n - 1];
+        }
+    }
+
     return 0;
 }
