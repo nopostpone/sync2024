9271eb1882edfcc8bd963a59d9fd82013b0a0e74
Csus4 <2334085989@qq.com>
2024-09-25 23:22:07 +0800

    <update> P11064 (todo)

--- sol/luogu/P11064.cpp
+++ sol/luogu/P11064.cpp
@@ -2,10 +2,32 @@
 using namespace std;
 using ll = long long;
 
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<ll> pre(n + 1);
+    for (int i = 0; i < n; i++) {
+        pre[i + 1] = pre[i] + a[i];
+    }
+
+    auto cmp = [&](int x, int y) {
+        
+    };
+}
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-    
-    
-    
+
+    int T;
+    for (cin >> T; T--;) {
+        solve();
+    }
+
     return 0;
 }
