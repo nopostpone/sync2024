3f9fcb9ffb9add823183ed65ba2d347f720b5d4f
Csus4 <2334085989@qq.com>
2024-10-04 04:45:59 +0800

    <update> P11064 (todo)

--- sol/luogu/P11064.cpp
+++ sol/luogu/P11064.cpp
@@ -67,40 +67,76 @@ struct SegmentTree {
     }
 };
 
-constexpr ll inf = -1e13;
-using pii = pair<int, int>;
+constexpr ll inf = 1e13;
 
-struct Info {
+struct Node {
     ll val = -inf;
     int l = 0;
     int r = 0;
 };
 
+constexpr Node operator+(const Node &a, const Node &b) {
+    // cerr << a.l << " " << a.r << " " << b.l << " " << b.r << endl;
+    assert(a.r == b.l or a.l == b.r);
+    return Node{a.val + b.val, min(a.l, b.l), max(a.r, b.r)};
+}
+
+bool check(const Node &x) {
+    return x.r == 0;
+}
+
+bool cmp2(const Node &a, const Node &b) {
+    return a.val == b.val ? (a.r - a.l) < (b.r - b.l) : a.val < b.val;
+}
+
+bool cmp1(const Node &a, const Node &b) {
+    return a.val == b.val ? (a.r - a.l) > (b.r - b.l) : a.val < b.val;
+}
+
 struct minInfo {
-    ll sum = -inf;
+    Node sum = Node();
+    Node pre = Node();
+    Node suf = Node();
+    Node ans = Node();
 };
 
 minInfo operator+(const minInfo &a, const minInfo &b) {
-    if (b.preidx == pii{}) {
-        return a;
-    } else if (a.preidx == pii{}) {
+    if (check(a.ans)) {
         return b;
+    } else if (check(b.ans)) {
+        return a;
     }
     minInfo c;
-    
 
+    c.pre = max(a.pre, a.sum + b.pre, cmp1);
+    c.suf = max(b.suf, b.sum + a.suf, cmp1);
+    c.sum = a.sum + b.sum;
+    c.ans = max({a.ans, b.ans, a.suf + b.pre}, cmp1);
+    
     return c;
 }
 
 struct maxInfo {
+    Node sum = Node();
+    Node pre = Node();
+    Node suf = Node();
+    Node ans = Node();
 };
 
 maxInfo operator+(const maxInfo &a, const maxInfo &b) {
-    if (b.preidx == pii{}) {
-        return a;
-    } else if (a.preidx == pii{}) {
+    if (check(a.ans)) {
         return b;
+    } else if (check(b.ans)) {
+        return a;
     }
+    maxInfo c;
+
+    c.pre = max(a.pre, a.sum + b.pre, cmp2);
+    c.suf = max(b.suf, b.sum + a.suf, cmp2);
+    c.sum = a.sum + b.sum;
+    c.ans = max({a.ans, b.ans, a.suf + b.pre}, cmp2);
+    
+    return c;
 }
 
 void solve() {
@@ -114,18 +150,34 @@ void solve() {
 
     vector<maxInfo> c(n);
     for (int i = 0; i < n; i++) {
+        c[i].ans = c[i].pre = c[i].suf = c[i].sum = Node{a[i], i, i + 1};
     }
 
+    cerr << 1 << endl;
     SegmentTree<maxInfo> Max(c);
+    cerr << 1 << endl;
     for (int i = 0; i < n; i++) {
+        auto t = Max.rangeQuery(0, n).ans;
+        cout << t.val << " \n"[i == n - 1];
+
+        for (int j = t.l; j < t.r; j++) {
+            Max.modify(j, maxInfo());
+        }
     }
 
     vector<minInfo> b(n);
     for (int i = 0; i < n; i++) {
+        b[i].ans = b[i].pre = b[i].suf = b[i].sum = Node{a[i], i, i + 1};
     }
 
     SegmentTree<minInfo> Min(b);
     for (int i = 0; i < n; i++) {
+        auto t = Min.rangeQuery(0, n).ans;
+        cout << t.val << " \n"[i == n - 1];
+
+        for (int j = t.l; j < t.r; j++) {
+            Max.modify(j, maxInfo());
+        }
     }
 }
 
