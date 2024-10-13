d3ecc6a04220e5097ba3e588a8253e0e9f7ea8fd
Csus4 <2334085989@qq.com>
2024-10-13 11:41:39 +0800

    <new> P9488

---
+++ sol/luogu/P9488.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+std::vector<int> minp, primes;
+
+void sieve(int n) {
+    minp.assign(n + 1, 0);
+    primes.clear();
+    
+    for (int i = 2; i < n; i++) {
+        if (minp[i] == 0) {
+            minp[i] = i;
+            primes.push_back(i);
+        }
+        
+        for (auto p : primes) {
+            if (i * p > n) {
+                break;
+            }
+            minp[i * p] = p;
+            if (p == minp[i]) {
+                break;
+            }
+        }
+    }
+}
+
+bool isprime(int n) {
+    return minp[n] == n;
+}
+
+struct DSU {
+    std::vector<int> f, siz;
+    
+    DSU() {}
+    DSU(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        f.resize(n);
+        std::iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
+    }
+    
+    int find(int x) {
+        while (x != f[x]) {
+            x = f[x] = f[f[x]];
+        }
+        return x;
+    }
+    
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+    
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        }
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    
+    int size(int x) {
+        return siz[find(x)];
+    }
+};
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+    sieve(n + 1);
+
+    DSU dsu(n + 1);
+    ll ans = 0;
+    for (int i = n / 2; i > 0; i--) {
+        for (int j : primes) {
+            if (1ll * i * j > n) {
+                break;
+            }
+            if (dsu.merge(i, i * j)) {
+                ans += i;
+            }
+        }
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
