294131c892907618d19f76348a30ae2c2119d659
Csus4 <2334085989@qq.com>
2024-10-13 20:23:42 +0800

    <update> P3384

--- sol/luogu/P3384.cpp
+++ sol/luogu/P3384.cpp
@@ -1,252 +1,271 @@
-// https://www.luogu.com.cn/problem/P3384
-// 树剖+线段树维护节点信息 模板题
+// HLD + LazySegmentTree 维护路在HLD中加入getPath和getTree，和数据结构解耦。径/子树信息
+// 24.10.13 upd: 
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define endl '\n'
-#define lst pos << 1
-#define rst pos << 1 | 1
-const int N = 1e5 + 10;
 
-vector<int> g[N];
-int n, root, m;
-int siz[N], top[N], hson[N], pre[N], dep[N], dfn[N], a[N], aa[N], bottom[N];
-int jiediandfn;
-ll P;
-struct SegmentTreeNode
-{
-    ll l, r, sum, add;
-} s[N * 4];
-vector<ll> res;
+int P;
 
-void Adfs(int now, int fa)
-{
-    int pmax = 0;
-    siz[now] = 1;
-    pre[now] = fa;
-    dep[now] = dep[fa] + 1;
-    for (auto &to : g[now])
-    {
-        if (to == fa)
-            continue;
-        Adfs(to, now);
-        if (siz[to] > siz[pmax])
-            pmax = to;
-        siz[now] += siz[to];
+struct HLD {
+    int n;
+    std::vector<int> siz, top, dep, parent, in, out, seq;
+    std::vector<std::vector<int>> adj;
+    int cur;
+
+    HLD() {}
+    HLD(int n) {
+        init(n);
     }
-    if (siz[now] == 1)
-        return;
-    hson[now] = pmax;
-}
-
-void Bdfs(int now, int tp)
-{
-    top[now] = tp;
-    dfn[now] = ++jiediandfn;
-    if (siz[now] == 1)
-        bottom[now] = dfn[now];
-
-    if (hson[now])
-    {
-        Bdfs(hson[now], tp);
-        bottom[now] = bottom[hson[now]];
+    void init(int n) {
+        this->n = n;
+        siz.resize(n);
+        top.resize(n);
+        dep.resize(n);
+        parent.resize(n);
+        in.resize(n);
+        out.resize(n);
+        seq.resize(n);
+        cur = 0;
+        adj.assign(n, {});
     }
-
-    for (auto &to : g[now])
-    {
-        if (to == pre[now] or to == hson[now])
-            continue;
-
-        Bdfs(to, to);
-        bottom[now] = max(bottom[now], bottom[to]);
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-}
-
-void pushdown(ll pos)
-{
-    s[lst].sum = (s[lst].sum + (s[lst].r - s[lst].l + 1) * s[pos].add) % P;
-    s[rst].sum = (s[rst].sum + (s[rst].r - s[rst].l + 1) * s[pos].add) % P;
-    s[lst].add += s[pos].add, s[rst].add += s[pos].add;
-    s[pos].add = 0;
-}
-
-void pushup(ll pos)
-{
-    s[pos].sum = s[lst].sum + s[rst].sum;
-    s[pos].sum %= P;
-}
-
-void build(ll l, ll r, ll pos)
-{
-    s[pos].l = l, s[pos].r = r, s[pos].add = 0;
-    if (l == r)
-    {
-        s[pos].sum = a[l] % P;
-        return;
+    void work(int root = 0) {
+        top[root] = root;
+        dep[root] = 0;
+        parent[root] = -1;
+        dfs1(root);
+        dfs2(root);
     }
-    ll m = l + ((r - l) >> 1);
-    build(l, m, lst);
-    build(m + 1, r, rst);
-    pushup(pos);
-}
-
-void add(ll x, ll y, ll pos, ll val)
-{
-    if (x > y)
-        swap(x, y);
-    if (x <= s[pos].l and s[pos].r <= y)
-    {
-        s[pos].add = (s[pos].add + val) % P;
-        s[pos].sum = (s[pos].sum + (s[pos].r - s[pos].l + 1) * val) % P;
-        return;
-    }
-    pushdown(pos);
-    ll m = (s[pos].l + s[pos].r) >> 1;
-    if (x <= m)
-        add(x, y, lst, val);
-    if (y > m)
-        add(x, y, rst, val);
-    pushup(pos);
-}
-
-ll getsum(ll x, ll y, ll pos)
-{
-    if (x > y)
-        swap(x, y);
-    if (x <= s[pos].l and s[pos].r <= y)
-    {
-        return (s[pos].sum) % P;
-    }
-    pushdown(pos);
-    ll m = (s[pos].l + s[pos].r) >> 1;
-    ll ans = 0;
-    if (x <= m)
-        ans = (ans + getsum(x, y, lst)) % P;
-    if (y > m)
-        ans = (ans + getsum(x, y, rst)) % P;
-    return ans;
-}
-
-void updatelujing(int u, int v, int val)
-{
-    while (top[u] != top[v])
-    {
-        if (dep[top[u]] < dep[top[v]])
-            swap(u, v);
-        add(dfn[top[u]], dfn[u], 1, val);
-        u = pre[top[u]];
-    }
-    add(dfn[u], dfn[v], 1, val);
-}
-
-ll asklujing(int u, int v)
-{
-    ll ans = 0;
-    while (top[u] != top[v])
-    {
-        if (dep[top[u]] < dep[top[v]])
-            swap(u, v);
-        ans += getsum(dfn[top[u]], dfn[u], 1);
-        ans %= P;
-        u = pre[top[u]];
-    }
-    ans += getsum(dfn[u], dfn[v], 1);
-    ans %= P;
-    return ans;
-}
-
-void updatezishu(int x, int z)
-{
-    add(dfn[x], bottom[x], 1, z);
-}
-
-ll askzishu(int x)
-{
-    return getsum(dfn[x], bottom[x], 1) % P;
-}
-
-void solve()
-{
-    cin >> n >> m >> root >> P;
-    for (int i = 1; i <= n; i++)
-        cin >> aa[i];
-    for (int i = 1; i < n; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-    Adfs(root, 0);
-    Bdfs(root, root);
-    for (int i = 1; i <= n; i++)
-        a[dfn[i]] = aa[i];
-    build(1, n, 1);
-    while (m--)
-    {
-        int opt, x, y = 0, z = 0;
-        cin >> opt;
-        if (opt == 1)
-        {
-            cin >> x >> y >> z;
-            updatelujing(x, y, z);
+    void dfs1(int u) {
+        if (parent[u] != -1) {
+            adj[u].erase(std::find(adj[u].begin(), adj[u].end(), parent[u]));
         }
-        else if (opt == 2)
-        {
-            cin >> x >> y;
-            res.push_back(asklujing(x, y));
-        }
-        else if (opt == 3)
-        {
-            cin >> x >> z;
-            updatezishu(x, z);
-        }
-        else if (opt == 4)
-        {
-            cin >> x;
-            res.push_back(askzishu(x));
-        }
-        else
-        {
-            cout << "?" << endl;
-            exit(0);
+
+        siz[u] = 1;
+        for (auto &v : adj[u]) {
+            parent[v] = u;
+            dep[v] = dep[u] + 1;
+            dfs1(v);
+            siz[u] += siz[v];
+            if (siz[v] > siz[adj[u][0]]) {
+                std::swap(v, adj[u][0]);
+            }
         }
     }
-    for (auto &i : res)
-        cout << i % P << endl;
+    void dfs2(int u) {
+        in[u] = cur++;
+        seq[in[u]] = u;
+        for (auto v : adj[u]) {
+            top[v] = v == adj[u][0] ? top[u] : v;
+            dfs2(v);
+        }
+        out[u] = cur;
+    }
+    int lca(int u, int v) {
+        while (top[u] != top[v]) {
+            if (dep[top[u]] > dep[top[v]]) {
+                u = parent[top[u]];
+            } else {
+                v = parent[top[v]];
+            }
+        }
+        return dep[u] < dep[v] ? u : v;
+    }
+    int dist(int u, int v) {
+        return dep[u] + dep[v] - 2 * dep[lca(u, v)];
+    }
+    bool isAncester(int fa, int son) {
+        return in[fa] <= in[son] && in[son] < out[fa];
+    }
+    auto getPath(int u, int v) {
+        vector<pair<int, int>> ret;
+        while (top[u] != top[v]) {
+            if (dep[top[u]] > dep[top[v]]) {
+                ret.push_back({in[top[u]], in[u]});
+                u = parent[top[u]];
+            } else {
+                ret.push_back({in[top[v]], in[v]});
+                v = parent[top[v]];
+            }
+        }
+        if (dep[u] > dep[v]) {
+            ret.push_back({in[v], in[u]});
+        } else {
+            ret.push_back({in[u], in[v]});
+        }
+        return ret;
+    }
+    pair<int, int> getTree(int u) {
+        return make_pair(in[u], out[u]);
+    }
+};
+
+template<class Info, class Tag>
+struct LazySegmentTree {
+    const int n;
+    std::vector<Info> info;
+    std::vector<Tag> tag;
+    LazySegmentTree(int n) : n(n), info(4 << std::__lg(n)), tag(4 << std::__lg(n)) {}
+    LazySegmentTree(std::vector<Info> init) : LazySegmentTree(init.size()) {
+        std::function<void(int, int, int)> build = [&](int p, int l, int r) {
+            if (r - l == 1) {
+                info[p] = init[l];
+                return;
+            }
+            int m = (l + r) / 2;
+            build(2 * p, l, m);
+            build(2 * p + 1, m, r);
+            pull(p);
+        };
+        build(1, 0, n);
+    }
+    void pull(int p) {
+        info[p] = info[2 * p] + info[2 * p + 1];
+    }
+    void apply(int p, const Tag &v) {
+        info[p].apply(v);
+        tag[p].apply(v);
+    }
+    void push(int p) {
+        apply(2 * p, tag[p]);
+        apply(2 * p + 1, tag[p]);
+        tag[p] = Tag();
+    }
+    void modify(int p, int l, int r, int x, const Info &v) {
+        if (r - l == 1) {
+            info[p] = v;
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        if (x < m) {
+            modify(2 * p, l, m, x, v);
+        } else {
+            modify(2 * p + 1, m, r, x, v);
+        }
+        pull(p);
+    }
+    void modify(int p, const Info &v) {
+        modify(1, 0, n, p, v);
+    }
+    Info rangeQuery(int p, int l, int r, int x, int y) {
+        if (l >= y || r <= x) {
+            return Info();
+        }
+        if (l >= x && r <= y) {
+            return info[p];
+        }
+        int m = (l + r) / 2;
+        push(p);
+        return rangeQuery(2 * p, l, m, x, y) + rangeQuery(2 * p + 1, m, r, x, y);
+    }
+    Info rangeQuery(int l, int r) {
+        return rangeQuery(1, 0, n, l, r);
+    }
+    void rangeApply(int p, int l, int r, int x, int y, const Tag &v) {
+        if (l >= y || r <= x) {
+            return;
+        }
+        if (l >= x && r <= y) {
+            apply(p, v);
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        rangeApply(2 * p, l, m, x, y, v);
+        rangeApply(2 * p + 1, m, r, x, y, v);
+        pull(p);
+    }
+    void rangeApply(int l, int r, const Tag &v) {
+        return rangeApply(1, 0, n, l, r, v);
+    }
+};
+struct Tag {
+    int add = 0;
+    void apply(const Tag &t) {
+        add = (add + t.add) % P;
+    }
+};
+struct Info {
+    int sum = 0;
+    int act = 0;
+    void apply(const Tag &t) {
+        sum = (1ull * t.add * act + sum) % P;
+    }
+};
+
+Info operator+(const Info &a, const Info &b) {
+    Info c;
+    c.sum = (a.sum + b.sum) % P;
+    c.act = a.act + b.act;
+    return c;
 }
 
-// 调线段树用-P3372
-void tiaoshi()
-{
-    P = 0x3f3f3f3f3f;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int n, m, rt;
+    cin >> n >> m >> rt >> P;
+    rt--;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    build(1, n, 1);
-    for (int i = 1; i <= m; i++)
-    {
-        int opt, x, y, k;
-        cin >> opt >> x >> y;
-        if (opt == 1)
-        {
-            cin >> k;
-            add(x, y, 1, k);
-        }
-        else if (opt == 2)
-            res.push_back(getsum(x, y, 1));
+    HLD hld(n);
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        hld.addEdge(u, v);
     }
-    for (auto &i : res)
-        cout << i << endl;
-}
+    hld.work(rt);
 
-int main()
-{
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int _ = 1;
-    while (_--)
-        solve();
+    vector<Info> init(n);
+    for (int i = 0; i < n; i++) {
+        init[hld.in[i]].sum = a[i];
+        init[hld.in[i]].act = 1;
+    }
+    LazySegmentTree<Info, Tag> seg(init);
+
+    while (m--) {
+        int op;
+        cin >> op;
+        if (op <= 2) {
+            int x, y;
+            cin >> x >> y;
+            x--, y--;
+            auto b = hld.getPath(x, y);
+            if (op == 1) {
+                int z;
+                cin >> z;
+                for (auto [u, v] : b) {
+                    seg.rangeApply(u, v + 1, {z % P});
+                }
+            } else {
+                int ans = 0;
+                for (auto [u, v] : b) {
+                    ans = (ans + seg.rangeQuery(u, v + 1).sum) % P;
+                }
+                cout << ans << "\n";
+            }
+        } else {
+            int x;
+            cin >> x;
+            x--;
+            auto [u, v] = hld.getTree(x);
+            if (op == 3) {
+                int z;
+                cin >> z;
+                seg.rangeApply(u, v, {z % P});
+            } else {
+                cout << (seg.rangeQuery(u, v).sum % P) << "\n";
+            }
+        }
+    }
+    
     return 0;
 }
