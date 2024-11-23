e66fd8d9c30a0a434acf0253440cb276cff953e4
Csus4 <2334085989@qq.com>
2024-11-23 13:22:22 +0800

    <update> P1908

--- sol/luogu/P1908.cpp
+++ sol/luogu/P1908.cpp
@@ -1,105 +1,78 @@
-// https://www.luogu.com.cn/problem/P1908
-// 权值线段树
-// 逆序对只关注相对大小关系，因此先做离散化
-// 然后从小到大一个一个插入线段树中，每插入一次，统计(id+1, n)中有多少个数，即有多少个值大于自己，且编号小于自己（因为按从小到大顺序），最后相加即为结果
+// 逆序对
+// 24.11.23 upd: 改成了权值树状数组
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define endl '\n'
-#define lst pos << 1
-#define rst pos << 1 | 1
-#define enter putchar('\n')
-const int N = 5e5 + 100;
 
-int n;
-int a[N], b[N];
-ll p;
-
-struct segtree
-{
-    ll l, r, sum;
-} s[N * 4];
-
-void pushup(ll pos)
-{
-    s[pos].sum = s[lst].sum + s[rst].sum;
-}
-
-void build(ll l, ll r, ll pos)
-{
-    s[pos].l = l, s[pos].r = r;
-    if (l == r)
-    {
-        s[pos].sum = 0;
-        return;
+template <typename T>
+struct Fenwick {
+    int n;
+    std::vector<T> a;
+    
+    Fenwick(int n_ = 0) {
+        init(n_);
     }
-    ll m = (l + r) >> 1;
-    build(l, m, lst);
-    build(m + 1, r, rst);
-    pushup(pos);
-}
-
-void upd(ll pos, ll c)
-{
-    if (s[pos].l == s[pos].r)
-    {
-        s[pos].sum++;
-        return;
+    
+    void init(int n_) {
+        n = n_;
+        a.assign(n, T{});
     }
-    ll m = s[pos].l + s[pos].r >> 1;
-    if (c <= m)
-        upd(lst, c);
-    else
-        upd(rst, c);
-    pushup(pos);
-    return;
-}
-
-ll query(ll pos, ll x, ll y)
-{
-    if (x <= s[pos].l and s[pos].r <= y)
-    {
-        return s[pos].sum;
+    
+    void add(int x, const T &v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] = a[i - 1] + v;
+        }
     }
-    ll m = s[pos].l + s[pos].r >> 1;
-    ll ans = 0;
-    if (x <= m)
-        ans += query(lst, x, y);
-    if (m < y)
-        ans += query(rst, x, y);
-    return ans;
-}
+    
+    T sum(int x) {
+        T ans{};
+        for (int i = x; i > 0; i -= i & -i) {
+            ans = ans + a[i - 1];
+        }
+        return ans;
+    }
+    
+    T rangeSum(int l, int r) {
+        return sum(r) - sum(l);
+    }
+    
+    int select(const T &k) {
+        int x = 0;
+        T cur{};
+        for (int i = 1 << std::__lg(n); i; i /= 2) {
+            if (x + i <= n && cur + a[x + i - 1] <= k) {
+                x += i;
+                cur = cur + a[x - 1];
+            }
+        }
+        return x;
+    }
+};
 
-ll res;
-
-void solve()
-{
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int n;
     cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    for (int i = 1; i <= n; i++)
-    {
-        b[i] = a[i];
-    }
-    sort(b + 1, b + n + 1);
-    build(1, n, 1);
-    for (int i = 1; i <= n; i++)
-    {
-        int id = lower_bound(b + 1, b + n + 1, a[i]) - b;
-        // cout << id << endl;
-        upd(1, id);
-        res += query(1, id + 1, n);
-    }
-    cout << res;
-}
 
-int main()
-{
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int _ = 1;
-    while (_--)
-        solve();
+    vector b(a);
+    ranges::sort(b);
+    
+    Fenwick<int> f(n);
+
+    ll res = 0;
+    for (int i = 0; i < n; i++) {
+        int id = lower_bound(b.begin(), b.end(), a[i]) - b.begin();
+        f.add(id, 1);
+        res += f.rangeSum(id + 1, n);
+    }
+
+    cout << res << endl;
+    
     return 0;
 }
