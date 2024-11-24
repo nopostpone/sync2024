6dbab9fd6c51d78cdcbb427d5d8591cb4c625987
Csus4 <2334085989@qq.com>
2024-11-24 19:51:58 +0800

    <update> abc381_g

--- sol/Atcoder/abc381_g.cpp
+++ sol/Atcoder/abc381_g.cpp
@@ -1,69 +1,218 @@
-#include <bits/stdc++.h>
-using namespace std;
-const long long MOD = 998244353;
-const long long HALF = 499122177;
-template <typename T>
-struct binary_indexed_tree {
-    int N;
-    vector<T> BIT;
-    binary_indexed_tree(int N) : N(N), BIT(N + 1, 0) {
-    }
-    void add(int i, T x) {
-        i++;
-        while (i <= N) {
-            BIT[i] += x;
-            i += i & -i;
+    #include <bits/stdc++.h>
+    using namespace std;
+    using ll = long long;
+
+    using ull = unsigned long long;
+    using uint = unsigned int;
+    template<typename T>
+    constexpr T power(T a, ull b) {
+        T res {1};
+        for (; b != 0; b /= 2, a *= a) {
+            if (b % 2 == 1) {
+                res *= a;
+            }
         }
+        return res;
     }
-    T sum(int i) {
-        T ans = 0;
-        while (i > 0) {
-            ans += BIT[i];
-            i -= i & -i;
+
+    template<uint P>
+    constexpr uint mulMod(uint a, uint b) {
+        return 1ULL * a * b % P;
+    }
+
+    template<ull P>
+    constexpr ull mulMod(ull a, ull b) {
+        ull res = a * b - ull(1.L * a * b / P - 0.5L) * P;
+        res %= P;
+        return res;
+    }
+
+    template<typename U, U P>
+    requires std::unsigned_integral<U>
+    struct ModIntBase {
+    public:
+        constexpr ModIntBase() : x(0) {}
+
+        template<typename T>
+        requires std::integral<T>
+        constexpr ModIntBase(T x_) : x(norm(x_ % T {P})) {}
+
+        constexpr static U norm(U x) {
+            if ((x >> (8 * sizeof(U) - 1) & 1) == 1) {
+                x += P;
+            }
+            if (x >= P) {
+                x -= P;
+            }
+            return x;
         }
-        return ans;
-    }
-    T sum(int L, int R) {
-        return sum(R) - sum(L);
-    }
-};
-int main() {
-    int N, K;
-    cin >> N >> K;
-    vector<int> P(N);
-    for (int i = 0; i < N; i++) {
-        cin >> P[i];
-        P[i]--;
-    }
-    vector<long long> inv(N + 1);
-    inv[1] = 1;
-    for (int i = 2; i <= N; i++) {
-        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-    }
-    binary_indexed_tree<long long> BIT(N);
-    long long inv0 = 0;
-    for (int i = 0; i < N; i++) {
-        inv0 += BIT.sum(P[i] + 1, N);
-        BIT.add(P[i], 1);
-    }
-    long long sum = 0;
-    long long inv1 = 0;
-    binary_indexed_tree<long long> BIT2(N);
-    for (int i = 0; i < K; i++) {
-        inv1 += BIT2.sum(P[i] + 1, N);
-        BIT2.add(P[i], 1);
-    }
-    for (int i = 0; i <= N - K; i++) {
-        sum += (inv0 - inv1 + (long long)K * (K - 1) / 2 % MOD * HALF % MOD + MOD) % MOD;
-        sum %= MOD;
-        if (i < N - K) {
-            inv1 += BIT2.sum(P[i + K] + 1, N);
-            BIT2.add(P[i + K], 1);
-            inv1 -= BIT2.sum(P[i]);
-            BIT2.add(P[i], -1);
+
+        constexpr U val() const {
+            return x;
         }
+
+        constexpr ModIntBase operator-() const {
+            ModIntBase res;
+            res.x = norm(P - x);
+            return res;
+        }
+
+        constexpr ModIntBase inv() const {
+            return power(*this, P - 2);
+        }
+
+        constexpr ModIntBase &operator*=(const ModIntBase &rhs) & {
+            x = mulMod<P>(x, rhs.val());
+            return *this;
+        }
+
+        constexpr ModIntBase &operator+=(const ModIntBase &rhs) & {
+            x = norm(x + rhs.x);
+            return *this;
+        }
+
+        constexpr ModIntBase &operator-=(const ModIntBase &rhs) & {
+            x = norm(x - rhs.x);
+            return *this;
+        }
+
+        constexpr ModIntBase &operator/=(const ModIntBase &rhs) & {
+            return *this *= rhs.inv();
+        }
+
+        friend constexpr ModIntBase operator*(ModIntBase lhs, const ModIntBase &rhs) {
+            lhs *= rhs;
+            return lhs;
+        }
+
+        friend constexpr ModIntBase operator+(ModIntBase lhs, const ModIntBase &rhs) {
+            lhs += rhs;
+            return lhs;
+        }
+
+        friend constexpr ModIntBase operator-(ModIntBase lhs, const ModIntBase &rhs) {
+            lhs -= rhs;
+            return lhs;
+        }
+
+        friend constexpr ModIntBase operator/(ModIntBase lhs, const ModIntBase &rhs) {
+            lhs /= rhs;
+            return lhs;
+        }
+
+        friend constexpr std::ostream &operator<<(std::ostream &os, const ModIntBase &a) {
+            return os << a.val();
+        }
+
+        friend constexpr bool operator==(ModIntBase lhs, ModIntBase rhs) {
+            return lhs.val() == rhs.val();
+        }
+
+        friend constexpr bool operator!=(ModIntBase lhs, ModIntBase rhs) {
+            return lhs.val() != rhs.val();
+        }
+
+        friend constexpr bool operator<(ModIntBase lhs, ModIntBase rhs) {
+            return lhs.val() < rhs.val();
+        }
+
+    private:
+        U x;
+    };
+
+    template<uint P>
+    using ModInt = ModIntBase<uint, P>;
+
+    template<ull P>
+    using ModInt64 = ModIntBase<ull, P>;
+
+    constexpr uint P = 998244353;
+    using Z = ModInt<P>;
+
+    template <typename T>
+    struct Fenwick {
+        int n;
+        std::vector<T> a;
+
+        Fenwick(int n_ = 0) {
+            init(n_);
+        }
+
+        void init(int n_) {
+            n = n_;
+            a.assign(n, T{});
+        }
+
+        void add(int x, const T &v) {
+            for (int i = x + 1; i <= n; i += i & -i) {
+                a[i - 1] = a[i - 1] + v;
+            }
+        }
+
+        T ori(int x) {
+            T ans{};
+            for (int i = x; i > 0; i -= i & -i) {
+                ans = ans + a[i - 1];
+            }
+            return ans;
+        }
+
+        T rangeSum(int l, int r) {
+            return ori(r) - ori(l);
+        }
+
+        int select(const T &k) {
+            int x = 0;
+            T cur{};
+            for (int i = 1 << std::__lg(n); i; i /= 2) {
+                if (x + i <= n && cur + a[x + i - 1] <= k) {
+                    x += i;
+                    cur = cur + a[x - 1];
+                }
+            }
+            return x;
+        }
+    };
+
+    int main() {
+        cin.tie(nullptr)->sync_with_stdio(false);
+
+        int n, k;
+        cin >> n >> k;
+
+        const Z E = Z(k) * (k - 1) / Z(4);
+
+        vector<int> p(n);
+        for (int i = 0; i < n; i++) {
+            cin >> p[i];
+            p[i]--;
+        }
+
+        Fenwick<int> f(n);
+
+        Z ori {};
+        for (int i = 0; i < n; i++) {
+            f.add(p[i], 1);
+            ori += f.rangeSum(p[i] + 1, n);
+        }
+
+        Fenwick<int> g(n);
+
+        Z now {};
+        for (int i = 0; i < k; i++) {
+            g.add(p[i], 1);
+            now += g.rangeSum(p[i] + 1, n);
+        }
+        Z res(now);
+        for (int i = k; i < n; i++) {
+            g.add(p[i - k], -1);
+            now -= g.rangeSum(0, p[i - k]);
+            now += g.rangeSum(p[i], n);
+            g.add(p[i], 1);
+
+            res += now;
+        }
+        cout << ori - res / (n - k + 1) + E << endl;
+
+        return 0;
     }
-    sum *= inv[N - K + 1];
-    sum %= MOD;
-    cout << sum << endl;
-}
