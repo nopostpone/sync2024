17cd46a14752807f13ec69dbb72e856b621b3e82
Csus4 <2334085989@qq.com>
2024-11-28 20:36:31 +0800

    <update> abc353_c

--- sol/Atcoder/abc353_c.cpp
+++ sol/Atcoder/abc353_c.cpp
@@ -2,80 +2,90 @@
 using namespace std;
 using ll = long long;
 
-struct DSU {
-    std::vector<int> f, siz;
-    DSU() {}
-    DSU(int n) {
-        init(n);
+template <typename T>
+struct Fenwick {
+    int n;
+    std::vector<T> a;
+    
+    Fenwick(int n_ = 0) {
+        init(n_);
     }
-    void init(int n) {
-        f.resize(n);
-        std::iota(f.begin(), f.end(), 0);
-        siz.assign(n, 1);
+    
+    void init(int n_) {
+        n = n_;
+        a.assign(n, T{});
     }
-    int find(int x) {
-        while (x != f[x]) {
-            x = f[x] = f[f[x]];
+    
+    void add(int x, const T &v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] = a[i - 1] + v;
+        }
+    }
+    
+    T sum(int x) {
+        T ans{};
+        for (int i = x; i > 0; i -= i & -i) {
+            ans = ans + a[i - 1];
+        }
+        return ans;
+    }
+    
+    T rangeSum(int l, int r) {
+        return sum(r) - sum(l);
+    }
+    
+    int select(const T &k) {
+        int x = 0;
+        T cur{};
+        for (int i = 1 << std::__lg(n); i; i /= 2) {
+            if (x + i <= n && cur + a[x + i - 1] <= k) {
+                x += i;
+                cur = cur + a[x - 1];
+            }
         }
         return x;
-    }
-    bool same(int x, int y) {
-        return find(x) == find(y);
-    }
-    bool merge(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x == y) {
-            return false;
-        }
-        siz[x] += siz[y];
-        f[y] = x;
-        return true;
-    }
-    int size(int x) {
-        return siz[find(x)];
     }
 };
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     
-    int n, q;
-    cin >> n >> q;
+    int n;
+    cin >> n;
 
-    DSU dsu(n);
-
-    vector<int> cnt(n, 1), C(n);
-    iota(C.begin(), C.end(), 0);
-
-    while (q--) {
-        int o;
-        cin >> o;
-
-        if (o == 1) {
-            int x, c;
-            cin >> x >> c;
-            x--, c--;
-
-            x = dsu.find(x);
-            cnt[C[x]] -= dsu.size(x);
-            cnt[C[x] = c] += dsu.size(x);
-
-            if (x > 0 and C[dsu.find(x - 1)] == c) {
-                dsu.merge(x - 1, x);
-                x = dsu.find(x);
-            }
-            if (x + dsu.size(x) < n and C[x + dsu.size(x)] == c) {
-                dsu.merge(x, x + dsu.size(x));
-            }
-        } else {
-            int c;
-            cin >> c;
-            c--;
-
-            cout << cnt[c] << "\n";
-        }
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
+
+    vector<ll> pre(n + 1);
+    for (int i = 1; i <= n; i++) {
+        pre[i] = pre[i - 1] + a[i - 1];
+    }
+
+    auto f = [&](int x, int y) {
+        return pre[y] - pre[x - 1];
+    };
+
+    vector<int> b(a);
+    ranges::sort(b);
+
+    Fenwick<int> fen(2 * n);
+
+    vector<int> cnt(n);
+    for (int i = n - 1; ~i; i--) {
+        int x = lower_bound(b.begin(), b.end(), 1e8 - a[i]) - b.begin();
+        cnt[i] = fen.rangeSum(x, n);
+        int id = lower_bound(b.begin(), b.end(), a[i]) - b.begin();
+        fen.add(id, 1);
+    }
+
+    ll ans = 0;
+    for (int i = 1; i < n; i++) {
+        ll tem = f(i + 1, n) + 1ll * a[i - 1] * (n - i) - 1ll * cnt[i - 1] * 1e8;
+        ans += tem;
+    }
+    cout << ans << endl;
     
     return 0;
 }
