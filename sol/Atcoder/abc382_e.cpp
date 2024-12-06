9789f667c6b41bf19cfffb3668ed112653d4c984
Csus4 <2334085989@qq.com>
2024-12-07 05:09:18 +0800

    <update> abc382_e

--- sol/Atcoder/abc382_e.cpp
+++ sol/Atcoder/abc382_e.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 using ll = long long;
 
+double f[5001];
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
@@ -16,27 +18,32 @@ int main() {
 
     vector dp0(n + 1, vector<double>(n + 1));
     dp0[0][0] = 1;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            dp0[i][j] += dp0[i - 1][j] * (1 - p[i - 1]);
-            dp0[i][j + 1] += dp0[i - 1][j] * p[i - 1]; 
+            dp0[i + 1][j] += dp0[i][j] * (1 - p[i]);
+            dp0[i + 1][j + 1] += dp0[i][j] * (p[i]);
         }
     }
 
     // 开一个箱子，获得 0~n 个稀有牌的概率
-    auto f = dp0[n];
+
+    // auto f = dp0[n];
+    for (int i = 0; i <= n; i++) {
+        f[i] = dp0[n][i];
+    }
 
     // 获得 0~x 个稀有牌，开箱子数量的期望
     vector<double> g(x + 1);
     for (int i = 1; i <= x; i++) {
-        double sum{1.}, dex{f[0]};
-        for (int j = 0; j < i; j++) {
+        double sum{};
+        for (int j = 1; j < i; j++) {
             sum += f[j] * g[i - j];
         }
-        g[i] = sum / (1 - dex);
+        g[i] = (sum + 1.) / (1. - f[0]);
     }
 
-    cout << fixed << setprecision(20) << g[x] << "\n";
+    cout << fixed << setprecision(10) << g[x] << "\n";
 
     return 0;
 }
+
