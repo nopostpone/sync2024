d18c8df4d649167b7361ce321e7752c43a3770d0
Csus4 <2334085989@qq.com>
2024-12-14 02:34:55 +0800

    <update> template : ModInteger

--- template/ModuleInteger.cpp
+++ template/ModuleInteger.cpp
@@ -1,8 +1,8 @@
 using ull = unsigned long long;
 using uint = unsigned int;
-template<typename T>
+template <typename T>
 constexpr T power(T a, ull b) {
-    T res {1};
+    T res{1};
     for (; b != 0; b /= 2, a *= a) {
         if (b % 2 == 1) {
             res *= a;
@@ -11,28 +11,28 @@ constexpr T power(T a, ull b) {
     return res;
 }
 
-template<uint P>
+template <uint P>
 constexpr uint mulMod(uint a, uint b) {
     return 1ULL * a * b % P;
 }
 
-template<ull P>
+template <ull P>
 constexpr ull mulMod(ull a, ull b) {
     ull res = a * b - ull(1.L * a * b / P - 0.5L) * P;
     res %= P;
     return res;
 }
 
-template<typename U, U P>
-requires std::unsigned_integral<U>
+template <typename U, U P>
+    requires std::unsigned_integral<U>
 struct ModIntBase {
 public:
     constexpr ModIntBase() : x(0) {}
-    
-    template<typename T>
-    requires std::integral<T>
-    constexpr ModIntBase(T x_) : x(norm(x_ % T {P})) {}
-    
+
+    template <typename T>
+        requires std::integral<T>
+    constexpr ModIntBase(T x_) : x(norm(x_ % T{P})) {}
+
     constexpr static U norm(U x) {
         if ((x >> (8 * sizeof(U) - 1) & 1) == 1) {
             x += P;
@@ -42,84 +42,70 @@ public:
         }
         return x;
     }
-    
+
     constexpr U val() const {
         return x;
     }
-    
     constexpr ModIntBase operator-() const {
         ModIntBase res;
         res.x = norm(P - x);
         return res;
     }
-    
     constexpr ModIntBase inv() const {
         return power(*this, P - 2);
     }
-    
     constexpr ModIntBase &operator*=(const ModIntBase &rhs) & {
         x = mulMod<P>(x, rhs.val());
         return *this;
     }
-    
     constexpr ModIntBase &operator+=(const ModIntBase &rhs) & {
         x = norm(x + rhs.x);
         return *this;
     }
-    
     constexpr ModIntBase &operator-=(const ModIntBase &rhs) & {
         x = norm(x - rhs.x);
         return *this;
     }
-    
     constexpr ModIntBase &operator/=(const ModIntBase &rhs) & {
         return *this *= rhs.inv();
     }
-    
     friend constexpr ModIntBase operator*(ModIntBase lhs, const ModIntBase &rhs) {
         lhs *= rhs;
         return lhs;
     }
-    
     friend constexpr ModIntBase operator+(ModIntBase lhs, const ModIntBase &rhs) {
         lhs += rhs;
         return lhs;
     }
-    
     friend constexpr ModIntBase operator-(ModIntBase lhs, const ModIntBase &rhs) {
         lhs -= rhs;
         return lhs;
     }
-    
     friend constexpr ModIntBase operator/(ModIntBase lhs, const ModIntBase &rhs) {
         lhs /= rhs;
         return lhs;
     }
-    
     friend constexpr std::ostream &operator<<(std::ostream &os, const ModIntBase &a) {
         return os << a.val();
     }
-    
     friend constexpr bool operator==(ModIntBase lhs, ModIntBase rhs) {
         return lhs.val() == rhs.val();
     }
-    
     friend constexpr bool operator!=(ModIntBase lhs, ModIntBase rhs) {
         return lhs.val() != rhs.val();
     }
-    
     friend constexpr bool operator<(ModIntBase lhs, ModIntBase rhs) {
         return lhs.val() < rhs.val();
     }
-    
+
 private:
     U x;
 };
 
-template<uint P>
+template <uint P>
 using ModInt = ModIntBase<uint, P>;
 
-template<ull P>
+template <ull P>
 using ModInt64 = ModIntBase<ull, P>;
 
 constexpr uint P = 1000000007;
