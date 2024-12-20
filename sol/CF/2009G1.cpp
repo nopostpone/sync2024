5a5cf33506f320359c6aacbcdf3ccbaa56d49eb1
Csus4 <2334085989@qq.com>
2024-12-20 15:42:52 +0800

    <update> 2009G1 更改为权值线段树做法

--- sol/CF/2009G1.cpp
+++ sol/CF/2009G1.cpp
@@ -2,43 +2,159 @@
 using namespace std;
 using ll = long long;
 
+template <class Info>
+struct SegmentTree {
+    int n;
+    std::vector<Info> info;
+    SegmentTree() : n(0) {}
+    SegmentTree(int n_, Info v_ = Info()) {
+        init(std::vector<Info>(n_, v_));
+    }
+    template <class T>
+    SegmentTree(std::vector<T> init_) {
+        init(init_);
+    }
+    template <class T>
+    void init(std::vector<T> init_) {
+        n = init_.size();
+        info.assign(4 << std::__lg(n), Info());
+        std::function<void(int, int, int)> build = [&](int p, int l, int r) {
+            if (r - l == 1) {
+                info[p] = init_[l];
+                return;
+            }
+            int m = (l + r) / 2;
+            build(2 * p, l, m);
+            build(2 * p + 1, m, r);
+            pull(p);
+        };
+        build(1, 0, n);
+    }
+    void pull(int p) {
+        info[p] = info[2 * p] + info[2 * p + 1];
+    }
+    void modify(int p, int l, int r, int x, const Info &v) {
+        if (r - l == 1) {
+            info[p] = v;
+            return;
+        }
+        int m = (l + r) / 2;
+        if (x < m) {
+            modify(2 * p, l, m, x, v);
+        } else {
+            modify(2 * p + 1, m, r, x, v);
+        }
+        pull(p);
+    }
+    void modify(int p, const Info &v) {
+        modify(1, 0, n, p, v);
+    }
+    Info rangeQuery(int p, int l, int r, int x, int y) {
+        if (l >= y || r <= x) {
+            return Info();
+        }
+        if (l >= x && r <= y) {
+            return info[p];
+        }
+        int m = (l + r) / 2;
+        return rangeQuery(2 * p, l, m, x, y) + rangeQuery(2 * p + 1, m, r, x, y);
+    }
+    Info rangeQuery(int l, int r) {
+        return rangeQuery(1, 0, n, l, r);
+    }
+    template <class F>
+    int findFirst(int p, int l, int r, int x, int y, F &&pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        int m = (l + r) / 2;
+        int res = findFirst(2 * p, l, m, x, y, pred);
+        if (res == -1) {
+            res = findFirst(2 * p + 1, m, r, x, y, pred);
+        }
+        return res;
+    }
+    template <class F>
+    int findFirst(int l, int r, F &&pred) {
+        return findFirst(1, 0, n, l, r, pred);
+    }
+    template <class F>
+    int findLast(int p, int l, int r, int x, int y, F &&pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        int m = (l + r) / 2;
+        int res = findLast(2 * p + 1, m, r, x, y, pred);
+        if (res == -1) {
+            res = findLast(2 * p, l, m, x, y, pred);
+        }
+        return res;
+    }
+    template <class F>
+    int findLast(int l, int r, F &&pred) {
+        return findLast(1, 0, n, l, r, pred);
+    }
+};
+
+struct Info {
+    int cnt{};
+};
+
+Info operator+(const Info &a, const Info &b) {
+    return {max(a.cnt, b.cnt)};
+}
+
+using L = SegmentTree<Info>;
+
 void solve() {
     int n, k, q;
     cin >> n >> k >> q;
 
     vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    
-    map<int, int> f;
-    multiset<int> s;
-    
     for (int i = 0; i < n; i++) {
-        s.insert(0);
+        cin >> a[i];
+        a[i] = a[i] - i + n - 1;
     }
+
+    vector<Info> init(2 * n);
     for (int i = 0; i < k - 1; i++) {
-        s.extract(f[a[i] - i]);
-        f[a[i] - i]++;
-        s.insert(f[a[i] - i]);
+        init[a[i]].cnt++;
     }
-    
+    L seg(init);
+
     vector<int> ans(n);
     for (int i = k - 1; i < n; i++) {
-        s.extract(f[a[i] - i]);
-        f[a[i] - i]++;
-        s.insert(f[a[i] - i]);
-        
+        auto v = seg.rangeQuery(a[i], a[i] + 1);
+        v.cnt++;
+        seg.modify(a[i], v);
+
         int p = i - k + 1;
-        ans[p] = k - *s.rbegin();
-        s.extract(f[a[p] - p]);
-        f[a[p] - p]--;
-        s.insert(f[a[p] - p]);
+        ans[p] = k - seg.rangeQuery(0, 2 * n).cnt;
+        
+        auto w = seg.rangeQuery(a[p], a[p] + 1);
+        if (w.cnt > 0) {
+            w.cnt--;
+        }
+        seg.modify(a[p], w);
     }
-    
+
     while (q--) {
-        int l, r;
-        cin >> l >> r;
-        cout << ans[l - 1] << endl;
+        int x, y;
+        cin >> x >> y;
+        x--, y--;
+        cout << ans[x] << "\n";
     }
 }
 
