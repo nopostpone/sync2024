5178e4f3042f74ca343e536c0154e5aa962678cd
Csus4 <2334085989@qq.com>
2024-12-22 19:50:46 +0800

    <update> cf1895_c

--- sol/CF/1895C.cpp
+++ sol/CF/1895C.cpp
@@ -8,52 +8,48 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> a(n);
+    array<vector<string>, 6> f;
     for (int i = 0; i < n; i++) {
-        cin >> a[i];
+        string s;
+        cin >> s;
+
+        f[s.size()].push_back(s);
+    }
+    ll ans{};
+    for (int x = 1; x <= 5; x++) {
+        for (int y = 1; y <= 5; y++) {
+            if ((x + y) % 2 == 1) {
+                continue;
+            }
+            int h = (x + y) / 2;
+
+            array<int, 92> cnt{};
+            for (auto a : f[x]) {
+                int s = 45;
+                for (int i = 0; i < x; i++) {
+                    if (i < h) {
+                        s += a[i] - '0';
+                    } else {
+                        s -= a[i] - '0';
+                    }
+                }
+                cnt[s] += 1;
+            }
+            for (auto a : f[y]) {
+                int s = 45;
+                for (int i = 0; i < y; i++) {
+                    if (x + i >= h) {
+                        s += a[i] - '0';
+                    } else {
+                        s -= a[i] - '0';
+                    }
+                }
+                ans += cnt[s];
+            }
+        }
     }
 
-    vector<ll> pre(n + 1);
-    for (int i = 0; i < n; i++) {
-        pre[i + 1] = pre[i] + a[i];
-    }
-
-    vector<ll> ppre(n + 1);
-    for (int i = 1; i <= n; i++) {
-        ppre[i] = ppre[i - 1] + pre[i];
-    }
-
-    vector<ll> f(n + 1);
-    for (int i = 1; i <= n; i++) {
-        f[i] = f[i - 1] + ppre[n] - ppre[i - 1] - pre[i - 1] * (n - i + 1);
-    }
-
-    vector<ll> siz(n + 1);
-    for (int i = 1; i <= n; i++) {
-        siz[i] = siz[i - 1] + n - i + 1;
-    }
-
-    auto sum = [&](ll x) -> ll {
-        int u = lower_bound(siz.begin() + 1, siz.end(), x) - siz.begin();
-        u--;
-
-        ll y = u + x - siz[u];
-
-        ll res = f[u];
-        res += ppre[y] - ppre[u] - pre[u] * (y - u);
-
-        return res;
-    };
-
-    int q;
-    cin >> q;
-
-    while (q--) {
-        ll l, r;
-        cin >> l >> r;
-
-        cout << sum(r) - sum(l - 1) << "\n";
-    }
+    cout << ans << "\n";
 
     return 0;
 }
