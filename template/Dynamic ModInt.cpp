e5612931f3a34bd4fc89a1f20985988e7f6092b8
Csus4 <2334085989@qq.com>
2025-01-09 01:09:07 +0800

    <update> template : Dynamic ModInt

--- template/Dynamic ModInt.cpp
+++ template/Dynamic ModInt.cpp
@@ -1,22 +1,177 @@
-using ll = long long;
-using ull = unsigned long long;
-using uint = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+using u128 = unsigned __int128;
+
+template <class T>
+constexpr T power(T a, u64 b, T res = 1) {
+    for (; b != 0; b /= 2, a *= a) {
+        if (b & 1) {
+            res *= a;
+        }
+    }
+    return res;
+}
+
+template <u32 P>
+constexpr u32 mulMod(u32 a, u32 b) {
+    return u64(a) * b % P;
+}
+
+template <u64 P>
+constexpr u64 mulMod(u64 a, u64 b) {
+    u64 res = a * b - u64(1.L * a * b / P - 0.5L) * P;
+    res %= P;
+    return res;
+}
+
+constexpr i64 safeMod(i64 x, i64 m) {
+    x %= m;
+    if (x < 0) {
+        x += m;
+    }
+    return x;
+}
+
+constexpr std::pair<i64, i64> invGcd(i64 a, i64 b) {
+    a = safeMod(a, b);
+    if (a == 0) {
+        return {b, 0};
+    }
+
+    i64 s = b, t = a;
+    i64 m0 = 0, m1 = 1;
+
+    while (t) {
+        i64 u = s / t;
+        s -= t * u;
+        m0 -= m1 * u;
+
+        std::swap(s, t);
+        std::swap(m0, m1);
+    }
+
+    if (m0 < 0) {
+        m0 += b / s;
+    }
+
+    return {s, m0};
+}
+
+template <std::unsigned_integral U, U P>
+struct ModIntBase {
+public:
+    constexpr ModIntBase() : x(0) {}
+    template <std::unsigned_integral T>
+    constexpr ModIntBase(T x_) : x(x_ % mod()) {}
+    template <std::signed_integral T>
+    constexpr ModIntBase(T x_) {
+        using S = std::make_signed_t<U>;
+        S v = x_ % S(mod());
+        if (v < 0) {
+            v += mod();
+        }
+        x = v;
+    }
+
+    constexpr static U mod() {
+        return P;
+    }
+
+    constexpr U val() const {
+        return x;
+    }
+
+    constexpr ModIntBase operator-() const {
+        ModIntBase res;
+        res.x = (x == 0 ? 0 : mod() - x);
+        return res;
+    }
+
+    constexpr ModIntBase inv() const {
+        return power(*this, mod() - 2);
+    }
+
+    constexpr ModIntBase &operator*=(const ModIntBase &rhs) & {
+        x = mulMod<mod()>(x, rhs.val());
+        return *this;
+    }
+    constexpr ModIntBase &operator+=(const ModIntBase &rhs) & {
+        x += rhs.val();
+        if (x >= mod()) {
+            x -= mod();
+        }
+        return *this;
+    }
+    constexpr ModIntBase &operator-=(const ModIntBase &rhs) & {
+        x -= rhs.val();
+        if (x >= mod()) {
+            x += mod();
+        }
+        return *this;
+    }
+    constexpr ModIntBase &operator/=(const ModIntBase &rhs) & {
+        return *this *= rhs.inv();
+    }
+
+    friend constexpr ModIntBase operator*(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator+(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator-(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator/(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs /= rhs;
+        return lhs;
+    }
+
+    friend constexpr std::istream &operator>>(std::istream &is, ModIntBase &a) {
+        i64 i;
+        is >> i;
+        a = i;
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const ModIntBase &a) {
+        return os << a.val();
+    }
+
+    friend constexpr bool operator==(const ModIntBase &lhs, const ModIntBase &rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr std::strong_ordering operator<=>(const ModIntBase &lhs, const ModIntBase &rhs) {
+        return lhs.val() <=> rhs.val();
+    }
+
+private:
+    U x;
+};
+
+template <u32 P>
+using ModInt = ModIntBase<u32, P>;
+template <u64 P>
+using ModInt64 = ModIntBase<u64, P>;
 
 struct Barrett {
 public:
-    Barrett(uint m_) : m(m_), im((ull)(-1) / m_ + 1) {}
+    Barrett(u32 m_) : m(m_), im((u64)(-1) / m_ + 1) {}
 
-    constexpr uint mod() const {
+    constexpr u32 mod() const {
         return m;
     }
 
-    constexpr uint mul(uint a, uint b) const {
-        ull z = a;
+    constexpr u32 mul(u32 a, u32 b) const {
+        u64 z = a;
         z *= b;
 
-        ull x = ull((u128(z) * im) >> 64);
+        u64 x = u64((u128(z) * im) >> 64);
 
-        uint v = uint(z - x * m);
+        u32 v = u32(z - x * m);
         if (m <= v) {
             v += m;
         }
@@ -24,11 +179,11 @@ public:
     }
 
 private:
-    uint m;
-    ull im;
+    u32 m;
+    u64 im;
 };
 
-template <uint Id>
+template <u32 Id>
 struct DynModInt {
 public:
     constexpr DynModInt() : x(0) {}
@@ -43,15 +198,15 @@ public:
         x = v;
     }
 
-    constexpr static void setMod(uint m) {
+    constexpr static void setMod(u32 m) {
         bt = m;
     }
 
-    static uint mod() {
+    static u32 mod() {
         return bt.mod();
     }
 
-    constexpr uint val() const {
+    constexpr u32 val() const {
         return x;
     }
 
@@ -124,10 +279,12 @@ public:
     }
 
 private:
-    uint x;
+    u32 x;
     static Barrett bt;
 };
 
-constexpr int P = 998244353;
+template <u32 Id>
+Barrett DynModInt<Id>::bt = 998244353;
 
+constexpr u64 P = 998244353;
 using Z = ModInt64<P>;
