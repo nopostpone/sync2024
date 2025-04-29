2f41426a5356fedab357c69bcfe397984f2c7b32
Csus4 <2334085989@qq.com>
2025-04-30 01:58:40 +0800

    <update> cf gym 102307 d

--- sol/CF/g102307D.cpp
+++ sol/CF/g102307D.cpp
@@ -6,62 +6,90 @@ using u64 = unsigned long long;
 using u32 = unsigned;
 using u128 = unsigned __int128;
 
-using p32 = pair<int, int>;
-constexpr p32 operator+(p32 a, p32 b) {
-    return {a.first + b.first, a.second + b.second};
-}
-constexpr p32 operator-(p32 a, p32 b) {
-    return {a.first - b.first, a.second - b.second};
-}
+struct DSU {
+    std::vector<int> f, siz;
+
+    DSU() {}
+    DSU(int n) { init(n); }
+
+    void init(int n) {
+        f.resize(n);
+        std::iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
+    }
+    int find(int x) {
+        while (x != f[x]) {
+            x = f[x] = f[f[x]];
+        }
+        return x;
+    }
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        }
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    int size(int x) {
+        return siz[find(x)];
+    }
+};
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, q;
-    cin >> n >> q;
-
     string s;
     cin >> s;
 
-    vector<p32> pos(n + 1);
-    for (int i = 0, x = 0, y = 0; i < n; i++) {
-        if (s[i] == 'U') {
-            y++;
-        } else if (s[i] == 'D') {
-            y--;
-        } else if (s[i] == 'L') {
-            x--;
-        } else {
-            x++;
+    int n = s.size();
+    int sqrtn = std::sqrt(n);
+
+    vector dsu(sqrtn, DSU(n + 1));
+
+    int q;
+    cin >> q;
+
+    vector<array<int, 4>> que(q);
+    for (int i = 0; i < q; i++) {
+        int st, a, k;
+        char c;
+        cin >> st >> a >> k >> c;
+        st--;
+
+        que[q - i - 1] = {st, a, k, c - 'a'};
+    }
+
+    vector<bool> vis(n);
+    for (auto [i, a, k, c] : que) {
+        if (a >= sqrtn) {
+            for (int j = 0; j <= k; j++) {
+                int x = i + j * a;
+                if (not vis[x]) {
+                    vis[x] = true;
+                    s[x] = c + 'a';
+                }
+            }
+            continue;
         }
-        pos[i + 1] = {x, y};
-    }
 
-    map<p32, vector<int>> f;
-    for (int i = 0; i <= n; i++) {
-        f[pos[i]].push_back(i);
-    }
-
-    auto get = [&](p32 p, int l, int r) {
-        if (not f.contains(p)) {
-            return false;
+        int cur = i;
+        while ((cur - i) / a <= k and cur < n) {
+            if (not vis[cur]) {
+                vis[cur] = true;
+                s[cur] = c + 'a';
+            }
+            dsu[a].merge(min(n, cur + a), cur);
+            cur = dsu[a].find(cur);
         }
-        auto &vec = f[p];
-        auto it = lower_bound(vec.begin(), vec.end(), l);
-        return it != vec.end() and *it <= r;
-    };
-
-    while (q--) {
-        int x, y, l, r;
-        cin >> x >> y >> l >> r;
-        l--;
-
-        p32 que = {x, y};
-        bool ok = get(que, 0, l) or get(que, r, n) or get(pos[l] + pos[r] - que, l, r);
-
-        cout << (ok ? "YES" : "NO") << "\n";
     }
+    cout << s << "\n";
 
     return 0;
 }
