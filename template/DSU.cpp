33dab41493b85829706cee3dfc6f6033ec75ff81
Csus4 <2334085989@qq.com>
2025-05-05 03:02:24 +0800

    <update> tp : dsu merge 3 DSU template to 1 file

--- template/DSU.cpp
+++ template/DSU.cpp
@@ -1,13 +1,18 @@
-/// @brief 并查集
+#include <bits/stdc++.h>
+
+using i64 = long long;
+using u32 = unsigned;
+
+/// @brief 普通并查集
 struct DSU {
-    vector<int> f, siz;
+    std::vector<int> f, siz;
 
     DSU() {}
     DSU(int n) { init(n); }
 
     void init(int n) {
         f.resize(n);
-        iota(f.begin(), f.end(), 0);
+        std::iota(f.begin(), f.end(), 0);
         siz.assign(n, 1);
     }
     int find(int x) {
@@ -31,5 +36,114 @@ struct DSU {
     }
     int size(int x) {
         return siz[find(x)];
+    }
+};
+
+/// @brief 可撤销并查集
+struct DSU {
+    std::vector<std::pair<int &, int>> his;
+    std::vector<int> f, siz;
+    
+    DSU () {}
+    DSU(int n) {
+        init(n);
+    }
+
+    void init(int n) {
+        f.resize(n);
+        std::iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
+    }
+    void set(int &a, int b) {
+        his.emplace_back(a, a);
+        a = b;
+    }
+
+    int find(int x) {
+        while (x != f[x]) {
+            x = f[x];
+        }
+        return x;
+    }
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        }
+        if (siz[x] < siz[y]) {
+            std::swap(x, y);
+        }
+        set(siz[x], siz[x] + siz[y]);
+        set(f[y], x);
+        return true;
+    }
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+    int cur() {
+        return his.size();
+    }
+    void rollback(int t) {
+        while (his.size() > t) {
+            auto [x, y] = his.back();
+            x = y;
+            his.pop_back();
+        }
+    }
+};
+
+
+/// @brief 可撤销并查集 - 维护每个连通分量是否为二分图
+struct DSU {
+    std::vector<std::pair<int &, int>> his;
+    
+    int n;
+    std::vector<int> f, g, bip;
+    
+    DSU(int n_) : n(n_), f(n, -1), g(n), bip(n, 1) {}
+    
+    std::pair<int, int> find(int x) {
+        if (f[x] < 0) {
+            return {x, 0};
+        }
+        auto [u, v] = find(f[x]);
+        return {u, v ^ g[x]};
+    }
+    void set(int &a, int b) {
+        his.emplace_back(a, a);
+        a = b;
+    }
+    void merge(int a, int b, int &ans) {
+        auto [u, xa] = find(a);
+        auto [v, xb] = find(b);
+        int w = xa ^ xb ^ 1;
+        if (u == v) {
+            if (bip[u] && w) {
+                set(bip[u], 0);
+                ans--;
+            }
+            return;
+        }
+        if (f[u] > f[v]) {
+            std::swap(u, v);
+        }
+        ans -= bip[u];
+        ans -= bip[v];
+        set(bip[u], bip[u] && bip[v]);
+        set(f[u], f[u] + f[v]);
+        set(f[v], u);
+        set(g[v], w);
+        ans += bip[u];
+    }
+    int cur() {
+        return his.size();
+    }
+    void rollback(int t) {
+        while (his.size() > t) {
+            auto [x, y] = his.back();
+            x = y;
+            his.pop_back();
+        }
     }
 };
