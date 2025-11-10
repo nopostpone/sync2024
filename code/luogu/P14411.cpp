bebc75ec5348b86ed90a16c9ee7fdcf3a8ec23ae
Csus4 <2334085989@qq.com>
2025-11-10 23:11:04 +0800

    <new> luogu : p14411

---
+++ code/luogu/P14411.cpp
@@ -0,0 +1,369 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+struct DSU {
+    std::vector<int> f, siz;
+
+    DSU() {}
+    DSU(int n) {
+        init(n);
+    }
+
+    void init(int n) {
+        f.resize(n);
+        std::iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
+    }
+    int find(int x) {
+        while (x != f[x]) {
+            x = f[x] = f[f[x]];
+        }
+        return x;
+    }
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        }
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    int size(int x) {
+        return siz[find(x)];
+    }
+};
+
+struct HLD {
+    int n;
+    vector<int> siz, top, dep, parent, in, out, seq;
+    vector<vector<int>> adj;
+    int cur;
+
+    HLD() {}
+    HLD(int n) {
+        init(n);
+    }
+    void init(int n) {
+        this->n = n;
+        siz.resize(n);
+        top.resize(n);
+        dep.resize(n);
+        parent.resize(n);
+        in.resize(n);
+        out.resize(n);
+        seq.resize(n);
+        cur = 0;
+        adj.assign(n, {});
+    }
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    void work(int root = 0) {
+        top[root] = root;
+        dep[root] = 0;
+        parent[root] = -1;
+        dfs1(root);
+        dfs2(root);
+    }
+    void dfs1(int u) {
+        if (parent[u] != -1) {
+            adj[u].erase(find(adj[u].begin(), adj[u].end(), parent[u]));
+        }
+
+        siz[u] = 1;
+        for (auto &v : adj[u]) {
+            parent[v] = u;
+            dep[v] = dep[u] + 1;
+            dfs1(v);
+            siz[u] += siz[v];
+            if (siz[v] > siz[adj[u][0]]) {
+                swap(v, adj[u][0]);
+            }
+        }
+    }
+    void dfs2(int u) {
+        in[u] = cur++;
+        seq[in[u]] = u;
+        for (auto v : adj[u]) {
+            top[v] = v == adj[u][0] ? top[u] : v;
+            dfs2(v);
+        }
+        out[u] = cur;
+    }
+    int lca(int u, int v) {
+        while (top[u] != top[v]) {
+            if (dep[top[u]] > dep[top[v]]) {
+                u = parent[top[u]];
+            } else {
+                v = parent[top[v]];
+            }
+        }
+        return dep[u] < dep[v] ? u : v;
+    }
+    int dist(int u, int v) {
+        return dep[u] + dep[v] - 2 * dep[lca(u, v)];
+    }
+    bool isAncester(int fa, int son) {
+        return in[fa] <= in[son] && in[son] < out[fa];
+    }
+    auto getPath(int u, int v) {
+        vector<pair<int, int>> ret;
+        while (top[u] != top[v]) {
+            if (dep[top[u]] > dep[top[v]]) {
+                ret.push_back({in[top[u]], in[u]});
+                u = parent[top[u]];
+            } else {
+                ret.push_back({in[top[v]], in[v]});
+                v = parent[top[v]];
+            }
+        }
+        if (dep[u] > dep[v]) {
+            ret.push_back({in[v], in[u]});
+        } else {
+            ret.push_back({in[u], in[v]});
+        }
+        return ret;
+    }
+    pair<int, int> getTree(int u) {
+        return make_pair(in[u], out[u]);
+    }
+};
+
+template <class Info, class Tag>
+struct LazySegmentTree {
+    int n;
+    std::vector<Info> info;
+    std::vector<Tag> tag;
+
+    LazySegmentTree() = delete;
+    LazySegmentTree(int n_, const Info &v_ = {}) { init(std::vector<Info>(n_, v_)); }
+    template <class T>
+    LazySegmentTree(const std::vector<T> &data) { init(data); }
+
+    template <class T>
+    void init(const std::vector<T> &data) {
+        n = data.size();
+        info.assign(4 << std::__lg(n), {});
+        tag.assign(4 << std::__lg(n), {});
+
+        auto build = [&](auto self, int p, int l, int r) -> void {
+            if (r - l == 1) {
+                info[p] = data[l];
+                return;
+            }
+            int m = (l + r) / 2;
+            self(self, 2 * p, l, m);
+            self(self, 2 * p + 1, m, r);
+            pull(p);
+        };
+        build(build, 1, 0, n);
+    }
+
+    void pull(int p) {
+        info[p] = info[2 * p] + info[2 * p + 1];
+    }
+    void apply(int p, const Tag &v) {
+        info[p].apply(v);
+        tag[p].apply(v);
+    }
+    void push(int p) {
+        apply(2 * p, tag[p]);
+        apply(2 * p + 1, tag[p]);
+        tag[p] = {};
+    }
+    void modify(int p, int l, int r, int x, const Info &v) {
+        if (r - l == 1) {
+            info[p] = v;
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        if (x < m) {
+            modify(2 * p, l, m, x, v);
+        } else {
+            modify(2 * p + 1, m, r, x, v);
+        }
+        pull(p);
+    }
+    void modify(int p, const Info &v) {
+        modify(1, 0, n, p, v);
+    }
+    Info rangeQuery(int p, int l, int r, int x, int y) {
+        if (l >= y || r <= x) {
+            return {};
+        }
+        if (l >= x && r <= y) {
+            return info[p];
+        }
+        int m = (l + r) / 2;
+        push(p);
+        return rangeQuery(2 * p, l, m, x, y) + rangeQuery(2 * p + 1, m, r, x, y);
+    }
+    Info rangeQuery(int l, int r) {
+        return rangeQuery(1, 0, n, l, r);
+    }
+    void rangeApply(int p, int l, int r, int x, int y, const Tag &v) {
+        if (l >= y || r <= x) {
+            return;
+        }
+        if (l >= x && r <= y) {
+            apply(p, v);
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        rangeApply(2 * p, l, m, x, y, v);
+        rangeApply(2 * p + 1, m, r, x, y, v);
+        pull(p);
+    }
+    void rangeApply(int l, int r, const Tag &v) {
+        return rangeApply(1, 0, n, l, r, v);
+    }
+    template <class F>
+    int findFirst(int p, int l, int r, int x, int y, const F &pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        push(p);
+        int m = (l + r) / 2;
+        int res = findFirst(2 * p, l, m, x, y, pred);
+        if (res == -1) {
+            res = findFirst(2 * p + 1, m, r, x, y, pred);
+        }
+        return res;
+    }
+    template <class F>
+    int findFirst(int l, int r, const F &pred) {
+        return findFirst(1, 0, n, l, r, pred);
+    }
+    template <class F>
+    int findLast(int p, int l, int r, int x, int y, const F &pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        push(p);
+        int m = (l + r) / 2;
+        int res = findLast(2 * p + 1, m, r, x, y, pred);
+        if (res == -1) {
+            res = findLast(2 * p, l, m, x, y, pred);
+        }
+        return res;
+    }
+    template <class F>
+    int findLast(int l, int r, const F &pred) {
+        return findLast(1, 0, n, l, r, pred);
+    }
+};
+
+struct Tag {
+    bool cov = false;
+    void apply(const Tag &t) {
+        cov |= t.cov;
+    }
+};
+
+struct Info {
+    int sum = 0;
+    int act = 0;
+    void apply(const Tag &t) {
+        if (t.cov) {
+            sum = act;
+        }
+    }
+};
+
+Info operator+(const Info &a, const Info &b) {
+    return {a.sum + b.sum, a.act + b.act};
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    DSU dsu(n);
+
+    vector<array<int, 3>> events;
+    HLD hld(n + 1);
+    for (int i = 0; i < q; i++) {
+        int t, a, b;
+        cin >> t >> a >> b;
+        a--;
+        b--;
+
+        if (t == 1 and dsu.merge(a, b)) {
+            hld.addEdge(a, b);
+        }
+
+        events.push_back({t, a, b});
+    }
+    for (int i = 0; i < n; i++) {
+        if (dsu.find(i) == i) {
+            hld.addEdge(n, i);
+        }
+    }
+
+    LazySegmentTree<Info, Tag> seg(n + 1, {0, 1});
+    hld.work(n);
+
+    dsu.init(n);
+    for (auto [t, u, v] : events) {
+        if (t == 1) {
+            if (not dsu.merge(u, v)) {
+                auto path = hld.getPath(u, v);
+                auto x = hld.in[hld.lca(u, v)];
+                for (auto [l, r] : path) {
+                    if (l == x) {
+                        l++;
+                    }
+                    seg.rangeApply(l, r + 1, {1});
+                }
+            }
+        } else {
+            int ans = -1;
+            if (dsu.same(u, v)) {
+                auto path = hld.getPath(u, v);
+                auto x = hld.in[hld.lca(u, v)];
+                ans = 0;
+                for (auto [l, r] : path) {
+                    if (l == x) {
+                        l++;
+                    }
+                    auto info = seg.rangeQuery(l, r + 1);
+                    ans += info.act - info.sum; 
+                }
+            }
+            cout << ans << "\n";
+        }
+
+    }
+
+    return 0;
+}
