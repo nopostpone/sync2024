2f7524b3cc196d763f8c2e5a061b38f19d369035
nopostpone <postpone@shu.edu.cn>
2026-01-02 18:36:18 +0800

    2026-01-02 abc438 g

---
+++ contest/atcoder/abc438/G_Sum_of_Min.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    i64 k;
+    cin >> n >> m >> k;
+
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+        
+
+    return 0;
+}
