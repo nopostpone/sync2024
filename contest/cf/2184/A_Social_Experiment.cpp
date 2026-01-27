921bbad96417cc68fe6489594028824378f92df0
nopostpone <postpone@shu.edu.cn>
2026-01-28 02:46:30 +0800

    2026-01-28 cf 2184

---
+++ contest/cf/2184/A_Social_Experiment.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int ans = n;
+
+    if (n > 3) {
+        ans = n % 2;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ contest/cf/2184/B_Hourglass.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+void solve() {
+    int s, k, m;
+    cin >> s >> k >> m;
+
+    if (s <= k) {
+        m %= k;
+        cout << max(s - m, 0) << "\n";
+    } else {
+        
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
