fbf941ec0f7ed637d8f9304c7c88e4f1513d4f15
nopostpone <postpone@shu.edu.cn>
2026-01-29 04:42:59 +0800

    2026-01-29 cf 664651

---
+++ contest/cf/664651/d.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << n + m + 1 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
+
+/*
+5
+0 0
+1 1
+3 7
+69 96
+114514 191981
+*/
