3732aaf55936206fad2441d885fc5b2db82f9502
nopostpone <postpone@shu.edu.cn>
2026-03-16 22:16:10 +0800

    2026-03-16 nowcoder 130168

---
+++ contest/nowcoder/130168/l.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+void solve() {
+    i64 n;
+    cin >> n;
+
+    cout << (n % 2 ? "Daiyousei" : "Cirno") << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
+
