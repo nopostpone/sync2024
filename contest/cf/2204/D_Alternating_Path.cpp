bfea271f9c625bff416474e4bc61dd3820bd6cda
nopostpone <postpone@shu.edu.cn>
2026-03-20 20:58:01 +0800

    2026-03-20 cf 2204 d

--- contest/cf/2204/D_Alternating_Path.cpp
+++ contest/cf/2204/D_Alternating_Path.cpp
@@ -8,83 +8,55 @@ using u64 = unsigned long long;
 using i128 = __int128;
 using u128 = unsigned __int128;
 
-struct DSU {
-    std::vector<int> f, siz;
-
-    DSU() {}
-    DSU(int n) {
-        init(n);
-    }
-
-    void init(int n) {
-        f.resize(n);
-        std::iota(f.begin(), f.end(), 0);
-        siz.assign(n, 1);
-    }
-    int find(int x) {
-        while (x != f[x]) {
-            x = f[x] = f[f[x]];
-        }
-        return x;
-    }
-    bool same(int x, int y) {
-        return find(x) == find(y);
-    }
-    bool merge(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x == y) {
-            return false;
-        }
-        siz[x] += siz[y];
-        f[y] = x;
-        return true;
-    }
-    int size(int x) {
-        return siz[find(x)];
-    }
-};
-
 void solve() {
     int n, m;
     cin >> n >> m;
 
-    DSU dsu(2 * n), g(n);
     vector<vector<int>> adj(n);
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
         u--;
         v--;
-        dsu.merge(u, v + n);
-        dsu.merge(u + n, v);
-        g.merge(u, v);
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     
     int ans = 0;
 
-    vector<bool> vis(n);
+    vector<int> vis(n, -1);
     for (int i = 0; i < n; i++) {
-        if (dsu.same(i, i + n) or vis[i]) {
+        if (vis[i] != -1) {
             continue;
         }
+
         int cnt[2] {};
-        [&](this auto &&self, int x, int t) -> void {
-            vis[x] = true;
-            cnt[t]++;
+        bool bip = true;
+
+        queue<int> q;
+        q.push(i);
+        vis[i] = 0;
+
+        while (not q.empty()) {
+            int x = q.front();
+            q.pop();
+            cnt[vis[x]]++;
 
             for (auto y : adj[x]) {
-                if (vis[y]) {
-                    continue;
+                if (vis[y] == -1) {
+                    vis[y] = 1 - vis[x];
+                    q.push(y);
                 }
-                self(y, t ^ 1);
+                if (vis[y] == vis[x]) {
+                    bip = false;
+                }
             }
-        } (i, 0);
-
-        ans += max(cnt[0], cnt[1]);
+        }
+        if (bip) {
+            ans += max(cnt[0], cnt[1]);
+        }
     }
+
     cout << ans << "\n";
 }
 
