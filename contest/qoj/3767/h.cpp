6a814f78721d24afade6730b93948389acbdcba0
nopostpone <postpone@shu.edu.cn>
2026-07-05 13:39:37 +0800

    upd h and i from qoj/3767

--- contest/qoj/3767/h.cpp
+++ contest/qoj/3767/h.cpp
@@ -1,130 +1,127 @@
 #include <bits/stdc++.h>
 
+using u32 = unsigned;
 using i64 = long long;
 
 constexpr int N = 8;
 
-int Mask[N];
+using B = std::bitset<N>;
 
 void solve()  {
     int n, m, k;
     std::cin >> n >> m >> k;
 
-    int mask = (1 << m) - 1;
-    int need = 0;
-
-    std::vector<int> f(n);
+    std::vector<B> grid(n);
     for (int i = 0; i < n; i++) {
         std::string s;
         std::cin >> s;
+
+        grid[i].set();
         for (int j = 0; j < m; j++) {
             if (s[j] == '.') {
-                f[i] |= 1 << j;
-                need++;
+                grid[i].reset(j);
             }
         }
     }
 
-    std::vector<std::vector<int>> g(k);
-    std::vector<int> r(k), c(k);
-    std::vector<int> suf(k + 1);
+    std::vector<std::array<B, N>> puz(k);
+    std::vector<int> r(k), c(k), tran(k);
     for (int i = 0; i < k; i++) {
         std::cin >> r[i] >> c[i];
 
-        g[i].resize(r[i]);
-
-        for (int j = 0; j < r[i]; j++) {
+        for (int x = 0; x < r[i]; x++) {
             std::string s;
             std::cin >> s;
 
-            for (int l = 0; l < c[i]; l++) {
-                if (s[l] == '#') {
-                    g[i][j] |= 1 << l;
-                    suf[i]++;
+            for (int y = 0; y < c[i]; y++) {
+                if (s[y] == '#') {
+                    puz[i][x].set(y);
                 }
             }
         }
-    }
-    for (int i = k - 1; i > 0; i--) {
-        suf[i - 1] += suf[i];
-    }
-
-    std::vector<int> cur(n);
-
-    std::vector<std::array<std::array<bool, N>, N>> v1(k);
-    for (int t = 0; t < k; t++) {
-        for (int ei = 0; ei + r[t] <= n; ei++) {
-            for (int ej = 0; ej + c[t] <= m; ej++) {
-                // valid ?
-                bool valid = true;
-                for (int i = 0; i < r[t]; i++) {
-                    int f1 = (f[ei + i] >> ej) & Mask[c[t]];
-                    int g1 = g[t][i];
-
-                    if ((f1 | g1) != f1) {
-                        valid = false;
-                    }
-                }
-
-                v1[t][ei][ej] = valid;
+        for (int y = 0; y < c[i]; y++) {
+            if (puz[i][0].test(y)) {
+                tran[i] = y;
+                break;
             }
         }
     }
+
+    std::vector<bool> used(k);
+
+    auto check = [&](int x, int y, int p) {
+        if (y < 0 or y >= m) {
+            return false;
+        }
+        if (x + r[p] > n or y + c[p] > m) {
+            return false;
+        }
+        for (int i = 0; i < r[p]; i++) {
+            for (int j = 0; j < c[p]; j++) {
+                if (puz[p][i].test(j) and grid[i + x].test(j + y)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+    auto fill = [&](int x, int y, int p, int t) {
+        for (int i = 0; i < r[p]; i++) {
+            for (int j = 0; j < c[p]; j++) {
+                if (puz[p][i].test(j)) {
+                    grid[i + x].set(j + y, t == 1);
+                }
+            }
+        }
+    };
 
     int ans = 0;
-    [&](this auto &&self, int dep) -> void {
-        if (cur == f) {
+
+    [&](this auto &&self) -> void {
+        int x, y;
+
+        bool ok = true;
+        for (int i = 0; i < n; i++) {
+            if (not grid[i].all()) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) {
             ans++;
             return;
         }
-        if (dep == k) {
-            return;
-        }
-        if (suf[dep] < need) {
-            return;
-        }
-
-        // not choose
-        self(dep + 1);
-        // choose
-        for (int ei = 0; ei + r[dep] <= n; ei++) {
-            for (int ej = 0; ej + c[dep] <= m; ej++) {
-                if (not v1[dep][ei][ej]) {
-                    continue;
+        bool found = false;
+        for (x = 0; x < n; x++) {
+            if (grid[x].all()) {
+                continue;
+            }
+            for (y = 0; y < m; y++) {
+                if (not grid[x].test(y)) {
+                    found = true;
+                    break;
                 }
-
-                // valid ?
-                bool valid = true;
-                for (int i = 0; i < r[dep]; i++) {
-                    int g1 = g[dep][i];
-                    int h1 = (cur[ei + i] >> ej) & Mask[c[dep]];
-                    h1 = Mask[c[dep]] ^ h1;
-
-                    if ((h1 | g1) != h1) {
-                        valid = false;
-                    }
-
-                }
-
-                if (not valid) {
-                    continue;
-                }
-
-                // push
-                auto old = cur;
-                need -= suf[dep] - suf[dep + 1];
-                for (int i = 0; i < r[dep]; i++) {
-                    cur[ei + i] |= g[dep][i] << ej;
-                }
-
-                self(dep + 1);
-                // undo
-                need += suf[dep] - suf[dep + 1];
-                cur = old;
+            }
+            if (found) {
+                break;
             }
         }
+        if (not found) {
+            return;
+        }
 
-    } (0);
+        for (int i = 0; i < k; i++) {
+            if (used[i] or not check(x, y - tran[i], i)) {
+                continue;
+            }
+            
+            used[i] = true;
+            fill(x, y - tran[i], i, 1);
+            self();
+            fill(x, y - tran[i], i, -1);
+            used[i] = false;
+        }
+    } ();
 
     std::cout << ans << "\n";
 }
@@ -135,10 +132,6 @@ int main() {
 
     int t;
     std::cin >> t;
-
-    for (int i = 0, x = 1; i <= N; i++, x *= 2) {
-        Mask[i] = x - 1;
-    }
 
     while (t--) {
         solve();
--- contest/qoj/3767/l.cpp
+++ contest/qoj/3767/l.cpp
@@ -1,15 +1,196 @@
 #include <bits/stdc++.h>
 
+using u32 = unsigned;
 using i64 = long long;
+using u64 = unsigned long long;
+
+using i128 = __int128;
+using u128 = unsigned __int128;
 
 namespace rgs = std::ranges;
+
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+i64 mul(i64 a, i64 b, i64 m) {
+    return static_cast<__int128>(a) * b % m;
+}
+
+i64 power(i64 a, i64 b, i64 m) {
+    i64 res = 1 % m;
+    for (; b; b >>= 1, a = mul(a, a, m))
+        if (b & 1)
+            res = mul(res, a, m);
+    return res;
+}
+
+bool isprime(i64 n) {
+    if (n < 2) {
+        return false;
+    }
+    static constexpr int A[] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+    int s = __builtin_ctzll(n - 1);
+    i64 d = (n - 1) >> s;
+    for (auto a : A) {
+        if (a == n) {
+            return true;
+        }
+        i64 x = power(a, d, n);
+        if (x == 1 or x == n - 1) {
+            continue;
+        }
+        bool ok = false;
+        for (int i = 0; i < s - 1; ++i) {
+            x = mul(x, x, n);
+            if (x == n - 1) {
+                ok = true;
+                break;
+            }
+        }
+        if (!ok) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<i64> factorize(i64 n) {
+    std::vector<i64> p;
+    [&](this auto &&self, i64 n) -> void {
+        if (n <= 10000) {
+            for (int i = 2; i * i <= n; i++) {
+                for (; n % i == 0; n /= i) {
+                    p.push_back(i);
+                }
+            }
+            if (n > 1) {
+                p.push_back(n);
+            }
+            return;
+        }
+        if (isprime(n)) {
+            p.push_back(n);
+            return;
+        }
+        auto g = [&](i64 x) {
+            return (mul(x, x, n) + 1) % n;
+        };
+        i64 x0 = 2;
+        while (true) {
+            i64 x = x0;
+            i64 y = x0;
+            i64 d = 1;
+            i64 power = 1, lam = 0;
+            i64 v = 1;
+            while (d == 1) {
+                y = g(y);
+                ++lam;
+                v = mul(v, std::abs(x - y), n);
+                if (lam % 127 == 0) {
+                    d = std::gcd(v, n);
+                    v = 1;
+                }
+                if (power == lam) {
+                    x = y;
+                    power *= 2;
+                    lam = 0;
+                    d = std::gcd(v, n);
+                    v = 1;
+                }
+            }
+            if (d != n) {
+                self(d);
+                self(n / d);
+                return;
+            }
+            ++x0;
+        }
+    } (n);
+
+    std::sort(p.begin(), p.end());
+    return p;
+}
 
 void solve()  {
     i64 a, b;
     int q;
     std::cin >> a >> b >> q;
 
+    bool swapped = false;
+    if (a > b) {
+        swapped = true;
+        std::swap(a, b);
+    }
+
+    i64 diff = b - a;
+
+    if (diff == 0) {
+        for (int i = 0; i < q; i++) {
+            i64 k;
+            std::cin >> k;
+
+            std::cout << 1 << " " << 1 << "\n";
+        }
+        return;
+    }
+    std::map<i64, int> pf;
+    int cnt = 0;
+
+    {
+        i64 td = diff;
+        auto fac = factorize(diff);
     
+        for (auto x : fac) {
+            while (td % x == 0) {
+                td /= x;
+                pf[x]++;
+                cnt++;
+            }
+        }
+        if (td != 1) {
+            pf[td]++;
+            cnt++;
+        }
+    }
+
+    i64 x = a;
+    i64 cur = 0;
+    std::vector<std::array<i64, 3>> events {{0, diff, x}};
+    while (cnt > 0) {
+        i64 m = 1e18;
+        for (auto [x1, y] : pf) {
+            if (y == 0) {
+                continue;
+            }
+            m = std::min(m, x1 - x % x1);
+        }
+        x += m;
+        cur += m;
+        for (auto &[x1, y] : pf) {
+            while (x % x1 == 0 and diff % x1 == 0) {
+                diff /= x1;
+                x /= x1;
+                y--;
+                cnt--;
+            }
+        }
+        events.push_back({cur, diff, x});
+    }
+
+    i64 max = 1e18;
+    for (int _ = 0; _ < q; _++) {
+        i64 k;
+        std::cin >> k;
+
+        auto [t, d, x] = *std::prev(std::upper_bound(events.begin(), events.end(), {k, max, max}));
+
+        x += k - t;
+
+        i64 a = x;
+        i64 b = x + d;
+        if (swapped) {
+            std::swap(a, b);
+        }
+        std::cout << a << " " << b << "\n";
+    }
 }
 
 int main() {
