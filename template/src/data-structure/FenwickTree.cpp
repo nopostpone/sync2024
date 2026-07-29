a99c668d73eedaef8454faa9b0c5ca4b68e20a6c
nopostpone <postpone@shu.edu.cn>
2026-07-30 01:47:31 +0800

    template update

--- template/src/data-structure/FenwickTree.cpp
+++ template/src/data-structure/FenwickTree.cpp
@@ -1,12 +1,11 @@
-template <typename T>
+template <class T>
 struct Fenwick {
     int n;
     std::vector<T> a;
-    Fenwick(int n_ = 0) {
-        init(n_);
+    Fenwick(int n = 0) {
+        this->init(n);
     }
-    void init(int n_) {
-        n = n_;
+    void init(int n) : n(n) {
         a.assign(n, T{});
     }
     void add(int x, const T &v) {
@@ -34,5 +33,39 @@ struct Fenwick {
             }
         }
         return x;
+    }
+};
+
+// 2-dimensional Fenwick
+template <class T>
+struct Fenwick {
+    int n, m;
+    std::vector<std::vector<T>> a;
+    Fenwick(int n = 0, int m = 0) {
+        init(n, m);
+    }
+    void init(int n, int m) {
+        this->n = n;
+        this->m = m;
+        a.assign(n, std::vector<T>(m, T{}));
+    }
+    void add(int x, int y, const T &v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            for (int j = y + 1; j <= m; j += j & -j) {
+                a[i - 1][j - 1] = a[i - 1][j - 1] + v;
+            }
+        }
+    }
+    T sum(int x, int y) {
+        T ans{};
+        for (int i = x; i > 0; i -= i & -i) {
+            for (int j = y; j > 0; j -= j & -j) {
+                ans = ans + a[i - 1][j - 1];
+            }
+        }
+        return ans;
+    }
+    T rangeSum(int x1, int y1, int x2, int y2) {
+        return sum(x2, y2) - sum(x1, y2) - sum(x2, y1) + sum(x1, y1);
     }
 };
